b772f1438171a915c11890786693eadff848f396
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-02-17 16:13:20 +0100

    Create fluid registry entity

---
+++ src/main/java/com/kozik/MPGK/entities/FluidRegistry.java
@@ -0,0 +1,28 @@
+package com.kozik.MPGK.entities;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "fluids_registry")
+@Data
+public class FluidRegistry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "registry_id", nullable = false)
+    private Long registryId;
+
+    @Column(name = "quantity", nullable = false)
+    private Long quantity;
+
+    @Column(name = "datetime", nullable = false)
+    private LocalDateTime datetime;
+}
