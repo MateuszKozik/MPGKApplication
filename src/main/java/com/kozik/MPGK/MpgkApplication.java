5c8697283c1e7acd42d334a9e27e785ad801c94d
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-02-17 15:36:42 +0100

    Disable Spring Security

--- src/main/java/com/kozik/MPGK/MpgkApplication.java
+++ src/main/java/com/kozik/MPGK/MpgkApplication.java
@@ -2,8 +2,9 @@ package com.kozik.MPGK;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class MpgkApplication {
 
 	public static void main(String[] args) {
