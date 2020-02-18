45729d7b083c3e85c2a00a22b569fe20671c0a48
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-02-18 12:44:18 +0100

    Create fluid registry repository

---
+++ src/main/java/com/kozik/MPGK/repositories/FluidRegistryRepository.java
@@ -0,0 +1,8 @@
+package com.kozik.MPGK.repositories;
+
+import com.kozik.MPGK.entities.FluidRegistry;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FluidRegistryRepository extends JpaRepository<FluidRegistry, Long>{
+
+}
