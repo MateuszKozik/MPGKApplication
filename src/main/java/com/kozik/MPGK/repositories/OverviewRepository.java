465d1f63bee3c0d354dbc3b179054a4ec2f6cf83
MichalStygar <misiek9753@o2.pl>
2020-02-18 12:54:41 +0100

    Create overview repository

---
+++ src/main/java/com/kozik/MPGK/repositories/OverviewRepository.java
@@ -0,0 +1,10 @@
+package com.kozik.MPGK.repositories;
+
+import com.kozik.MPGK.entities.Overview;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OverviewRepository extends JpaRepository<Overview, Long> {
+
+    
+}
