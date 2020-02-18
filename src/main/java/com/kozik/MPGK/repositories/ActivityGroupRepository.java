5695de32cffbc35df6f14361ee2b72baf0547dbc
MichalStygar <misiek9753@o2.pl>
2020-02-18 12:59:20 +0100

    Create activityGroup repository

---
+++ src/main/java/com/kozik/MPGK/repositories/ActivityGroupRepository.java
@@ -0,0 +1,10 @@
+package com.kozik.MPGK.repositories;
+
+import com.kozik.MPGK.entities.ActivityGroup;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActivityGroupRepository extends JpaRepository<ActivityGroup, Long> {
+
+    
+}
