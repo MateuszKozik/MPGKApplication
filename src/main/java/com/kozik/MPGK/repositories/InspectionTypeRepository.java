03a1ec2d51fa291ecc095219089de12712883d6c
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-02-18 13:16:59 +0100

    Create inspection type repository

---
+++ src/main/java/com/kozik/MPGK/repositories/InspectionTypeRepository.java
@@ -0,0 +1,8 @@
+package com.kozik.MPGK.repositories;
+
+import com.kozik.MPGK.entities.InspectionType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InspectionTypeRepository extends JpaRepository<InspectionType, Long>{
+    
+}
