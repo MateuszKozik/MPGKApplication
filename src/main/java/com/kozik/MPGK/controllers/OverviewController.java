b64e04ff08c7b2d251fcba70b7fcbdea3557854d
MichalStygar <misiek9753@o2.pl>
2020-02-20 14:40:59 +0100

    Add to overviewControler person in add funtion

--- src/main/java/com/kozik/MPGK/controllers/OverviewController.java
+++ src/main/java/com/kozik/MPGK/controllers/OverviewController.java
@@ -3,7 +3,9 @@ package com.kozik.MPGK.controllers;
 import java.util.List;
 
 import com.kozik.MPGK.entities.Overview;
+import com.kozik.MPGK.entities.Person;
 import com.kozik.MPGK.services.OverviewService;
+import com.kozik.MPGK.services.PersonService;
 
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +14,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class OverviewController {
 
     @Autowired private OverviewService overviewService;
+
+    @Autowired private PersonService personService;
 
     @GetMapping(value = "/overview/list")
     public String getAll(Model model){
@@ -27,13 +32,17 @@ public class OverviewController {
 
     @GetMapping(value="/overview/add")
     public String add(Model model) {
+        List<Person> personList = personService.listAll();
         Overview overview = new Overview();
         model.addAttribute("overview", overview);
+        model.addAttribute("personList", personList);
         return "views/overview/add";
     }
 
     @PostMapping(value="/overview/add")
-    public String add(@ModelAttribute("overview")Overview overview){
+    public String add(@ModelAttribute("overview")Overview overview,
+    @RequestParam(name="persons")Person person){
+        overview.setPerson(person);
         overviewService.save(overview);
         return "redirect:/overview/list";
     }
