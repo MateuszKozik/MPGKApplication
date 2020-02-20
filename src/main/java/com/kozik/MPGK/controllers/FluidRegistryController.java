8b698da1d403d579ea982c4903178f2f9bd7d1d6
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-02-20 14:19:55 +0100

    Inject relation dependencies in the fluid registry controller

--- src/main/java/com/kozik/MPGK/controllers/FluidRegistryController.java
+++ src/main/java/com/kozik/MPGK/controllers/FluidRegistryController.java
@@ -2,8 +2,14 @@ package com.kozik.MPGK.controllers;
 
 import java.util.List;
 
+import com.kozik.MPGK.entities.Device;
+import com.kozik.MPGK.entities.Fluid;
 import com.kozik.MPGK.entities.FluidRegistry;
+import com.kozik.MPGK.entities.Person;
+import com.kozik.MPGK.services.DeviceService;
 import com.kozik.MPGK.services.FluidRegistryService;
+import com.kozik.MPGK.services.FluidService;
+import com.kozik.MPGK.services.PersonService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,11 +18,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FluidRegistryController {
 
     @Autowired private FluidRegistryService fluidRegistryService;
+    @Autowired private PersonService personService;
+    @Autowired private FluidService fluidService;
+    @Autowired private DeviceService deviceService;
 
     @GetMapping(value = "/fluidRegistry/list")
     public String getAll(Model model){
@@ -27,28 +37,52 @@ public class FluidRegistryController {
 
     @GetMapping(value = "/fluidRegistry/add")
     public String add(Model model){
+        List<Person> persons = personService.listAll();
+        List<Fluid> fluids = fluidService.listAll();
+        List<Device> devices = deviceService.listAll();
         FluidRegistry fluidRegistry = new FluidRegistry();
         model.addAttribute("fluidRegistry", fluidRegistry);
+        model.addAttribute("persons", persons);
+        model.addAttribute("fluids", fluids);
+        model.addAttribute("devices", devices);
         return "views/fluidRegistry/add";
     }
 
     @PostMapping(value = "/fluidRegistry/add")
-    public String add(@ModelAttribute("fluidRegistry")FluidRegistry fluidRegistry){
+    public String add(@ModelAttribute("fluidRegistry")FluidRegistry fluidRegistry,
+    @RequestParam(name = "person", required = false)Person person,
+    @RequestParam(name = "fluid", required = false)Fluid fluid,
+    @RequestParam(name = "device", required = false)Device device){
+        fluidRegistry.setPerson(person);
+        fluidRegistry.setFluid(fluid);
+        fluidRegistry.setDevice(device);
         fluidRegistryService.save(fluidRegistry);
         return "redirect:/fluidRegistry/list";
     }
     
     @GetMapping(value = "/fluidRegistry/edit/{id}")
     public String edit(@PathVariable("id")Long id, Model model){
+        List<Person> persons = personService.listAll();
+        List<Fluid> fluids = fluidService.listAll();
+        List<Device> devices = deviceService.listAll();
         FluidRegistry fluidRegistry = fluidRegistryService.get(id);
         model.addAttribute("fluidRegistry", fluidRegistry);
+        model.addAttribute("persons", persons);
+        model.addAttribute("fluids", fluids);
+        model.addAttribute("devices", devices);
         return "views/fluidRegistry/edit";
     }
 
     @PostMapping(value = "/fluidRegistry/edit/{id}")
     public String edit(@PathVariable("id")Long id,
-    @ModelAttribute("fluidRegistry")FluidRegistry fluidRegistry){
+    @ModelAttribute("fluidRegistry")FluidRegistry fluidRegistry,
+    @RequestParam(name = "person", required = false)Person person,
+    @RequestParam(name = "fluid", required = false)Fluid fluid,
+    @RequestParam(name = "device", required = false)Device device){
         fluidRegistry.setRegistryId(id);
+        fluidRegistry.setPerson(person);
+        fluidRegistry.setFluid(fluid);
+        fluidRegistry.setDevice(device);
         fluidRegistryService.save(fluidRegistry);
         return "redirect:/fluidRegistry/list";
     }
