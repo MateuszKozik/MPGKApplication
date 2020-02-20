7aefc1e0f808a608bce59d3b6d86bd567587a929
MichalStygar <misiek9753@o2.pl>
2020-02-20 10:33:10 +0100

    Add to userController save role

--- src/main/java/com/kozik/MPGK/controllers/UserController.java
+++ src/main/java/com/kozik/MPGK/controllers/UserController.java
@@ -1,7 +1,11 @@
 package com.kozik.MPGK.controllers;
 
+import java.util.HashSet;
 import java.util.List;
+
+import com.kozik.MPGK.entities.Role;
 import com.kozik.MPGK.entities.User;
+import com.kozik.MPGK.services.RoleService;
 import com.kozik.MPGK.services.UserService;
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +14,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
 
-    @Autowired private UserService userService;   
+    @Autowired private UserService userService;
+    @Autowired private RoleService roleService;   
     
     @GetMapping(value = "/user/list")
     public String getAll(Model model){
@@ -25,14 +31,17 @@ public class UserController {
 
     @GetMapping(value="/user/add")
     public String add(Model model) {
+        List<Role> roleList = roleService.listAll();
         User user = new User();
         model.addAttribute("user", user);
+        model.addAttribute("roleList", roleList);
         return "views/user/add";
     }
     
     @PostMapping(value="/user/add")
-    public String add(@ModelAttribute("user")User user){
-        userService.save(user);
+    public String add(@ModelAttribute("user")User user,
+        @RequestParam(name="roles")HashSet<Role> roles){
+        userService.save(user,roles);
         return "redirect:/user/list";
     }
 
