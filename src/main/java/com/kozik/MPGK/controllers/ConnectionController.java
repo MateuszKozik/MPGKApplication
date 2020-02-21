f3737f0a1eec8f846cc5574c5e42f71aa242c64d
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-02-21 12:08:10 +0100

    Inject relation dependencies in the connection controller

--- src/main/java/com/kozik/MPGK/controllers/ConnectionController.java
+++ src/main/java/com/kozik/MPGK/controllers/ConnectionController.java
@@ -2,8 +2,14 @@ package com.kozik.MPGK.controllers;
 
 import java.util.List;
 
+import com.kozik.MPGK.entities.ActivityGroup;
 import com.kozik.MPGK.entities.Connection;
+import com.kozik.MPGK.entities.Device;
+import com.kozik.MPGK.entities.InspectionType;
+import com.kozik.MPGK.services.ActivityGroupService;
 import com.kozik.MPGK.services.ConnectionService;
+import com.kozik.MPGK.services.DeviceService;
+import com.kozik.MPGK.services.InspectionTypeService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,11 +18,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ConnectionController {
 
     @Autowired private ConnectionService connectionService;
+    @Autowired private ActivityGroupService activityGroupService;
+    @Autowired private InspectionTypeService inspectionTypeService;
+    @Autowired private DeviceService deviceService;
 
     @GetMapping(value = "/connection/list")
     public String getAll(Model model){
@@ -27,28 +37,52 @@ public class ConnectionController {
 
     @GetMapping(value = "/connection/add")
     public String add(Model model){
+        List<ActivityGroup> activitiesGroups = activityGroupService.listAll();
+        List<InspectionType> inspectionsType = inspectionTypeService.listAll();
+        List<Device> devices = deviceService.listAll();
         Connection connection = new Connection();
         model.addAttribute("connection", connection);
+        model.addAttribute("activitiesGroups", activitiesGroups);
+        model.addAttribute("inspectionsType", inspectionsType);
+        model.addAttribute("devices", devices);
         return "views/connection/add";
     }
 
     @PostMapping(value = "/connection/add")
-    public String add(@ModelAttribute("connection")Connection connection){
+    public String add(@ModelAttribute("connection")Connection connection,
+    @RequestParam(name = "activityGroup", required = false)ActivityGroup activityGroup,
+    @RequestParam(name = "inspectionType", required = false)InspectionType inspectionType,
+    @RequestParam(name = "device", required = false)Device device){
+        connection.setActivityGroup(activityGroup);
+        connection.setInspectionType(inspectionType);
+        connection.setDevice(device);
         connectionService.save(connection);
         return "redirect:/connection/list";
     }
 
     @GetMapping(value = "/connection/edit/{id}")
     public String edit(@PathVariable("id")Long id, Model model){
+        List<ActivityGroup> activitiesGroups = activityGroupService.listAll();
+        List<InspectionType> inspectionsType = inspectionTypeService.listAll();
+        List<Device> devices = deviceService.listAll();
         Connection connection = connectionService.get(id);
         model.addAttribute("connection", connection);
+        model.addAttribute("activitiesGroups", activitiesGroups);
+        model.addAttribute("inspectionsType", inspectionsType);
+        model.addAttribute("devices", devices);
         return "views/connection/edit";
     }
 
     @PostMapping(value = "/connection/edit/{id}")
     public String edit(@PathVariable("id")Long id,
-    @ModelAttribute("connection")Connection connection){
+    @ModelAttribute("connection")Connection connection,
+    @RequestParam(name = "activityGroup", required = false)ActivityGroup activityGroup,
+    @RequestParam(name = "inspectionType", required = false)InspectionType inspectionType,
+    @RequestParam(name = "device", required = false)Device device){
         connection.setConnectionId(id);
+        connection.setActivityGroup(activityGroup);
+        connection.setInspectionType(inspectionType);
+        connection.setDevice(device);
         connectionService.save(connection);
         return "redirect:/connection/list";
     }
