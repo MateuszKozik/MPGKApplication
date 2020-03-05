0b12d31728a5ed9d866ef8d4c20e6059bb9a10df
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-03-05 19:13:36 +0100

    Add connection services to activity group controller

--- src/main/java/com/kozik/MPGK/controllers/ActivityGroupController.java
+++ src/main/java/com/kozik/MPGK/controllers/ActivityGroupController.java
@@ -3,7 +3,10 @@ package com.kozik.MPGK.controllers;
 import java.util.List;
 
 import com.kozik.MPGK.entities.ActivityGroup;
+import com.kozik.MPGK.entities.Connection;
 import com.kozik.MPGK.services.ActivityGroupService;
+import com.kozik.MPGK.services.ConnectionService;
+
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,11 +14,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ActivityGroupController {
 
     @Autowired private ActivityGroupService activityGroupService;
+    @Autowired private ConnectionService connectionService;
 
     @GetMapping(value = "/activityGroup/list")
     public String getAll(Model model){
@@ -26,28 +31,36 @@ public class ActivityGroupController {
 
     @GetMapping(value="/activityGroup/add")
     public String add(Model model) {
+        List<Connection> connections = connectionService.listAll();
         ActivityGroup activityGroup = new ActivityGroup();
         model.addAttribute("activityGroup", activityGroup);
+        model.addAttribute("connections", connections);
         return "views/activityGroup/add";
     }
     
     @PostMapping(value="/activityGroup/add")
-    public String add(@ModelAttribute("activityGroup")ActivityGroup activityGroup){
+    public String add(@ModelAttribute("activityGroup")ActivityGroup activityGroup,
+    @RequestParam(name = "connection", required = false)Connection connection){
+        activityGroup.setConnection(connection);
         activityGroupService.save(activityGroup);
         return "redirect:/activityGroup/list";
     }
 
     @GetMapping(value = "/activityGroup/edit/{id}")
     public String edit(@PathVariable("id")Long id, Model model){
+        List<Connection> connections = connectionService.listAll();
         ActivityGroup activityGroup = activityGroupService.get(id);
         model.addAttribute("activityGroup", activityGroup);
+        model.addAttribute("connections", connections);
         return "views/activityGroup/edit";
     }
 
     @PostMapping(value = "/activityGroup/edit/{id}")
     public String edit(@PathVariable("id")Long id,
-    @ModelAttribute("fluactivityGroupid")ActivityGroup activityGroup){
+    @ModelAttribute("activityGroup")ActivityGroup activityGroup,
+    @RequestParam(name = "connection", required = false)Connection connection){
         activityGroup.setGroupId(id);
+        activityGroup.setConnection(connection);
         activityGroupService.save(activityGroup);
         return "redirect:/activityGroup/list";
     }
