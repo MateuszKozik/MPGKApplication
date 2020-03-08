38392482f9b6f897a9df5a82a79539654e916e43
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-03-08 11:21:40 +0100

    Create a class to display errors

---
+++ src/main/java/com/kozik/MPGK/utilities/ErrorMessage.java
@@ -0,0 +1,13 @@
+package com.kozik.MPGK.utilities;
+
+public class ErrorMessage {
+    private String errorMessage;
+
+    public ErrorMessage(String errorMessage){
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }   
+}
