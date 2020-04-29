071006d46161c1d2b7e00607e800d8342c861c1d
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-04-29 18:50:51 +0200

    Configure react-redux

--- mpgk-react-frontend/src/App.js
+++ mpgk-react-frontend/src/App.js
@@ -5,16 +5,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Layout/Header";
 import DeviceList from "./components/Device/DeviceList";
 import AddDevice from "./components/Device/AddDevice";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
     return (
-        <Router>
-            <div className="App">
-                <Header />
-                <Route exact path="/devices" component={DeviceList} />
-                <Route exact path="/devices/add" component={AddDevice} />
-            </div>
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <div className="App">
+                    <Header />
+                    <Route exact path="/devices" component={DeviceList} />
+                    <Route exact path="/devices/add" component={AddDevice} />
+                </div>
+            </Router>
+        </Provider>
     );
 }
 
