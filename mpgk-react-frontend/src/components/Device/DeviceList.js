4930abf3a3aa26218557642d3de646e32b02255b
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-04-29 17:05:59 +0200

    Create device list component

---
+++ mpgk-react-frontend/src/components/Device/DeviceList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class DeviceList extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h1 className="display-4 text-center">Urządzenia</h1>
+            </div>
+        );
+    }
+}
+
+export default DeviceList;
