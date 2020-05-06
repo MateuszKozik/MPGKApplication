60905d4a9a42b33f59d7e8bc1d26ebe3f336006e
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-05-06 18:22:40 +0200

    Add fluids types

--- mpgk-react-frontend/src/actions/types.js
+++ mpgk-react-frontend/src/actions/types.js
@@ -4,3 +4,8 @@ export const GET_ERRORS = "GET_ERRORS";
 export const GET_DEVICES = "GET_DEVICES";
 export const GET_DEVICE = "GET_DEVICE";
 export const DELETE_DEVICE = "DELETE_DEVICE";
+
+//fluids
+export const GET_FLUIDS = "GET_FLUIDS";
+export const GET_FLUID = "GET_FLUID";
+export const DELETE_FLUID = "DELETE_FLUID";
