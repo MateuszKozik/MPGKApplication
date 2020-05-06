b35fdcb45b8346bf5ecb644db7b0fdd3c58a222c
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-05-06 20:17:17 +0200

    Add get all fluids and delete fluid functionality

--- mpgk-react-frontend/src/components/Fluid/FluidList.js
+++ mpgk-react-frontend/src/components/Fluid/FluidList.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
 import AddFluidButton from "./AddFluidButton";
+import { connect } from "react-redux";
+import { getFluids, deleteFluid } from "../../actions/fluidActions";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 class FluidList extends Component {
+    componentDidMount() {
+        this.props.getFluids();
+    }
+
+    onDeleteClick = (fluidId) => {
+        this.props.deleteFluid(fluidId);
+    };
+
     render() {
+        const { fluids } = this.props.fluid;
         return (
             <div className="container mt-2">
                 <h1 className="display-4 text-center mt-2">Płyny</h1>
@@ -10,18 +23,36 @@ class FluidList extends Component {
                     <AddFluidButton />
                 </div>
                 <div className="table-responsive mt-2">
-                    <table className="table table-bordered table-striped">
-                        <thead className="thead-dark">
-                            <tr className="active">
+                    <table className="table ">
+                        <thead>
+                            <tr>
                                 <th>Nazwa</th>
                                 <th>Akcje</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td> </td>
-                                <td></td>
-                            </tr>
+                            {fluids.map((fluid) => (
+                                <tr key={fluid.fluidId}>
+                                    <td>{fluid.name}</td>
+                                    <td>
+                                        <Link
+                                            to={`/fluids/update/${fluid.fluidId}`}
+                                            className="btn btn-primary my-1"
+                                        >
+                                            Edytuj
+                                        </Link>
+                                        <button
+                                            onClick={this.onDeleteClick.bind(
+                                                this,
+                                                fluid.fluidId
+                                            )}
+                                            className="btn btn-danger ml-2 my-1"
+                                        >
+                                            Usuń
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                 </div>
@@ -30,4 +61,14 @@ class FluidList extends Component {
     }
 }
 
-export default FluidList;
+FluidList.propTypes = {
+    fluid: PropTypes.object.isRequired,
+    getFluids: PropTypes.func.isRequired,
+    deleteFluid: PropTypes.func.isRequired
+};
+
+const mapStateToProps = (state) => ({
+    fluid: state.fluid
+});
+
+export default connect(mapStateToProps, { getFluids, deleteFluid })(FluidList);
