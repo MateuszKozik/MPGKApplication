4a933619b25e8593551be88c02328b159a859994
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-05-09 12:57:27 +0200

    Create overview type reducer

--- mpgk-react-frontend/src/actions/types.js
+++ mpgk-react-frontend/src/actions/types.js
@@ -9,3 +9,8 @@ export const DELETE_DEVICE = "DELETE_DEVICE";
 export const GET_FLUIDS = "GET_FLUIDS";
 export const GET_FLUID = "GET_FLUID";
 export const DELETE_FLUID = "DELETE_FLUID";
+
+//overview types
+export const GET_OVERVIEW_TYPES = "GET_OVERVIEW_TYPES";
+export const GET_OVERVIEW_TYPE = "GET_OVERVIEW_TYPE";
+export const DELETE_OVERVIEW_TYPE = "DELETE_OVERVIEW_TYPE";
--- mpgk-react-frontend/src/reducers/index.js
+++ mpgk-react-frontend/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import errorReducer from "./errorReducer";
 import deviceReducer from "./deviceReducer";
 import fluidReducer from "./fluidReducer";
+import overviewTypeReducer from "./overviewTypeReducer";
 
 export default combineReducers({
     errors: errorReducer,
     device: deviceReducer,
-    fluid: fluidReducer
+    fluid: fluidReducer,
+    overviewType: overviewTypeReducer
 });
