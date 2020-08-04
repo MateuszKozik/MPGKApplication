28f36b1d6ca9fc623d2608648f2f7f30693baa11
MichalStygar <misiek9753@o2.pl>
2020-08-04 22:27:45 +0200

    Change ActivityGroupController

--- mpgk-backend/src/main/java/com/kozik/MPGK/controllers/ActivityGroupController.java
+++ mpgk-backend/src/main/java/com/kozik/MPGK/controllers/ActivityGroupController.java
@@ -1,11 +1,9 @@
 package com.kozik.MPGK.controllers;
 
-import java.util.List;
+
 
 import com.kozik.MPGK.entities.ActivityGroup;
 import com.kozik.MPGK.services.ActivityGroupService;
-import com.kozik.MPGK.utilities.ErrorMessage;
-import org.springframework.http.HttpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,68 +15,58 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
+import com.kozik.MPGK.services.MapValidationErrorService;
+import javax.validation.Valid;
+import org.springframework.validation.BindingResult;
+import com.kozik.MPGK.utilities.Message;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/groups")
 public class ActivityGroupController {
 
     @Autowired private ActivityGroupService activityGroupService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     //Get all activities groups
-    @GetMapping("/groups")
-    public ResponseEntity<List<ActivityGroup>> getActivityGroups(){
-        List<ActivityGroup> activityGroups = activityGroupService.listAll();
-        if(activityGroups.isEmpty()){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<List<ActivityGroup>>(activityGroups, HttpStatus.OK);
+    @GetMapping("")
+    public Iterable<ActivityGroup> getActivityGroups() {
+        return activityGroupService.listAll();
     }
 
     //Get single activity group
-    @GetMapping("/groups/{groupId}")
-    public ResponseEntity<?> getActivityGroup(@PathVariable("groupId")Long groupId){
-        if(!activityGroupService.isActivityGroupExist(groupId)){
-            return new ResponseEntity<>(new ErrorMessage("Group with id " + groupId + " not found."),
-            HttpStatus.NOT_FOUND);
-        }
-        ActivityGroup activityGroup = activityGroupService.get(groupId);
-        return new ResponseEntity<ActivityGroup>(activityGroup, HttpStatus.OK);
+    @GetMapping("/{groupId}")
+    public ResponseEntity<?> getActivityGroup(@PathVariable Long groupId) {
+        return new ResponseEntity<ActivityGroup>(activityGroupService.get(groupId), HttpStatus.OK);
     }
 
     //Create activity group
-    @PostMapping("/groups")
-    public ResponseEntity<?> createActivityGroup(@RequestBody ActivityGroup activityGroup, UriComponentsBuilder builder){
-        Long groupId = activityGroup.getGroupId();
-        if(groupId != null){
-            return new ResponseEntity<>(new ErrorMessage("Unable to create. Group with id " + groupId 
-            + " already exist."), HttpStatus.CONFLICT);
+    @PostMapping("")
+    public ResponseEntity<?> createActivityGroup(@Valid @RequestBody ActivityGroup activityGroup, BindingResult result) {
+        if (result.hasErrors()) {
+            return mapValidationErrorService.MapValidationService(result);
         }
-        activityGroupService.save(activityGroup);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(builder.path("/api/groups/{groupId}").buildAndExpand(activityGroup.getGroupId()).toUri());
-        return new ResponseEntity<String>(headers, HttpStatus.CREATED);
+
+        return new ResponseEntity<ActivityGroup>(activityGroupService.save(activityGroup), HttpStatus.CREATED);
     }
 
     //Update activity group
-    @PutMapping("/groups/{groupId}")
-    public ResponseEntity<?> updateActivityGroup(@PathVariable("groupId")Long groupId, @RequestBody ActivityGroup activityGroup){
-        if(!activityGroupService.isActivityGroupExist(groupId)){
-            return new ResponseEntity<>(new ErrorMessage("Unable to update. Group with id " + groupId + " not found."),
-            HttpStatus.NOT_FOUND);
+    @PutMapping("/{groupId}")
+    public ResponseEntity<?> updateActivityGroup(@PathVariable Long groupId, @Valid @RequestBody ActivityGroup activityGroup,
+            BindingResult result) {
+        if (result.hasErrors()) {
+            return mapValidationErrorService.MapValidationService(result);
         }
-        ActivityGroup currentActivityGroup = activityGroupService.update(groupId,activityGroup);
-        return new ResponseEntity<ActivityGroup>(currentActivityGroup, HttpStatus.OK);
+
+        return new ResponseEntity<ActivityGroup>(activityGroupService.update(groupId, activityGroup), HttpStatus.OK);
     }
 
     //Delete activity group
-    @DeleteMapping("/groups/{groupId}")
-    public ResponseEntity<?> deleteActivityGroup(@PathVariable("groupId")Long groupId){
-        if(!activityGroupService.isActivityGroupExist(groupId)){
-            return new ResponseEntity<>(new ErrorMessage("Unable to delete. Group with id " + groupId + " not found"),
-            HttpStatus.NOT_FOUND);
-        }
+    @DeleteMapping("/{groupId}")
+    public ResponseEntity<?> deleteActivityGroup(@PathVariable("groupId") Long groupId) {
         activityGroupService.delete(groupId);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<Message>(new Message("Activity group with id: " + groupId + " has been removed."),
+                HttpStatus.OK);
     }
 }
