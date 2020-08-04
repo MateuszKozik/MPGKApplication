531dd2cd5fd32e04a86a5e8e29edd28156fd95e8
MichalStygar <misiek9753@o2.pl>
2020-08-04 21:25:15 +0200

    Change PersonController

--- mpgk-backend/src/main/java/com/kozik/MPGK/controllers/PersonController.java
+++ mpgk-backend/src/main/java/com/kozik/MPGK/controllers/PersonController.java
@@ -1,11 +1,12 @@
 package com.kozik.MPGK.controllers;
 
-import java.util.List;
 
+
+import javax.validation.Valid;
+import org.springframework.validation.BindingResult;
 import com.kozik.MPGK.entities.Person;
+import com.kozik.MPGK.services.MapValidationErrorService;
 import com.kozik.MPGK.services.PersonService;
-import com.kozik.MPGK.utilities.ErrorMessage;
-import org.springframework.http.HttpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,68 +18,56 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
+import com.kozik.MPGK.utilities.Message;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/persons")
 public class PersonController {
 
-    @Autowired private PersonService personService;
+    @Autowired 
+    private PersonService personService;
+
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
 
     //Get all persons
-    @GetMapping("/persons")
-    public ResponseEntity<List<Person>> getPersons(){
-        List<Person> persons = personService.listAll();
-        if(persons.isEmpty()){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<List<Person>>(persons, HttpStatus.OK);
+    @GetMapping("")
+    public Iterable<Person> getPersons() {
+        return personService.listAll();
     }
 
     //Get single person
-    @GetMapping("/persons/{personId}")
-    public ResponseEntity<?> getPerson(@PathVariable("personId")Long personId){
-        if(!personService.isPersonExist(personId)){
-            return new ResponseEntity<>(new ErrorMessage("Person with id " + personId + " not found."),
-            HttpStatus.NOT_FOUND);
-        }
-        Person person = personService.get(personId);
-        return new ResponseEntity<Person>(person, HttpStatus.OK);
+    @GetMapping("/{personId}")
+    public ResponseEntity<?> getPerson(@PathVariable Long personId) {
+        return new ResponseEntity<Person>(personService.get(personId), HttpStatus.OK);
     }
 
     //Create person
-    @PostMapping("/persons")
-    public ResponseEntity<?> createFluid(@RequestBody Person person, UriComponentsBuilder builder){
-        Long personId = person.getPersonId();
-        if(personId != null){
-            return new ResponseEntity<>(new ErrorMessage("Unable to create. Person with id " + personId 
-            + " already exist."), HttpStatus.CONFLICT);
+    @PostMapping("")
+    public ResponseEntity<?> createPerson(@Valid @RequestBody Person person, BindingResult result) {
+        if (result.hasErrors()) {
+            return mapValidationErrorService.MapValidationService(result);
         }
-        personService.save(person);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(builder.path("/api/persons/{personId}").buildAndExpand(person.getPersonId()).toUri());
-        return new ResponseEntity<String>(headers, HttpStatus.CREATED);
+
+        return new ResponseEntity<Person>(personService.save(person), HttpStatus.CREATED);
     }
 
     //Update person
-    @PutMapping("/persons/{personId}")
-    public ResponseEntity<?> updatePerson(@PathVariable("personId")Long personId, @RequestBody Person person){
-        if(!personService.isPersonExist(personId)){
-            return new ResponseEntity<>(new ErrorMessage("Unable to update. Person with id " + personId + " not found."),
-            HttpStatus.NOT_FOUND);
+    @PutMapping("/{personId}")
+    public ResponseEntity<?> updatePerson(@PathVariable Long personId, @Valid @RequestBody Person person,
+            BindingResult result) {
+        if (result.hasErrors()) {
+            return mapValidationErrorService.MapValidationService(result);
         }
-        Person currentPerson = personService.update(personId,person);
-        return new ResponseEntity<Person>(currentPerson, HttpStatus.OK);
+
+        return new ResponseEntity<Person>(personService.update(personId, person), HttpStatus.OK);
     }
 
     //Delete person
-    @DeleteMapping("/persons/{personId}")
-    public ResponseEntity<?> deletePerson(@PathVariable("personId")Long personId){
-        if(!personService.isPersonExist(personId)){
-            return new ResponseEntity<>(new ErrorMessage("Unable to delete. Perosn with id " + personId + " not found"),
-            HttpStatus.NOT_FOUND);
-        }
+    @DeleteMapping("{personId}")
+    public ResponseEntity<?> deletePerson(@PathVariable("personId") Long personId) {
         personService.delete(personId);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<Message>(new Message("Person with id: " + personId + " has been removed."),
+                HttpStatus.OK);
     }
 }
