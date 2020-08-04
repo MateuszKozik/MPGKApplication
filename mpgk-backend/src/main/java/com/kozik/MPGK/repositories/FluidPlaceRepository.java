af4bf7e8b793532359e43e6025de047e7738e065
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-08-04 20:24:28 +0200

    Create fluid place repository

---
+++ mpgk-backend/src/main/java/com/kozik/MPGK/repositories/FluidPlaceRepository.java
@@ -0,0 +1,9 @@
+package com.kozik.MPGK.repositories;
+
+import com.kozik.MPGK.entities.FluidPlace;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FluidPlaceRepository extends JpaRepository<FluidPlace, Long> {
+
+}
