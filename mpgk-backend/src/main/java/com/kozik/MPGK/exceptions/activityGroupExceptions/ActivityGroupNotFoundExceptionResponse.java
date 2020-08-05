ebb56357a6c5450f66002fc92b30623ee576d024
MichalStygar <misiek9753@o2.pl>
2020-08-05 15:56:13 +0200

    Add ActivityGroupNotFoundExceptionResponse

---
+++ mpgk-backend/src/main/java/com/kozik/MPGK/exceptions/activityGroupExceptions/ActivityGroupNotFoundExceptionResponse.java
@@ -0,0 +1,11 @@
+package com.kozik.MPGK.exceptions.activityGroupExceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ActivityGroupNotFoundExceptionResponse {
+
+    private String ActivityGroupNotFound;
+}
