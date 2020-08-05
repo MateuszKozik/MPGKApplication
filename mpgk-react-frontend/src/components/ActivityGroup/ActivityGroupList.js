24c8e48ac549f9ad4c5b8dc1f88da1ceb24f31a6
MichalStygar <misiek9753@o2.pl>
2020-08-05 19:34:27 +0200

    Create ActivityGroupList

---
+++ mpgk-react-frontend/src/components/ActivityGroup/ActivityGroupList.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import AddActivityGroupButton from "./AddActivityGroupButton";
+import { connect } from "react-redux";
+import { getGroups, deleteGroup } from "../../actions/activityGroupActions";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+class ActivityGroupList extends Component {
+    componentDidMount() {
+        this.props.getGroups();
+    }
+
+    onDeleteClick = (groupId) => {
+        this.props.deleteGroup(groupId);
+    };
+
+    render() {
+        const { groups } = this.props.group;
+        return (
+            <div className="container mt-2">
+                <h1 className="display-4 text-center mt-2">Grupy czynności</h1>
+                <div className="row">
+                    <div className="col-md-4 my-1">
+                        <AddFluidButton />
+                    </div>
+                </div>
+                <div className="table-responsive mt-2">
+                    <table className="table ">
+                        <thead>
+                            <tr>
+                                <th>Nazwa</th>
+                                <th>Akcje</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {groups.map((group) => (
+                                <tr key={group.groupId}>
+                                    <td>{group.name}</td>
+                                    <td>
+                                        <Link
+                                            to={`/groups/update/${group.groupId}`}
+                                            className="btn btn-primary my-1"
+                                        >
+                                            Edytuj
+                                        </Link>
+                                        <button
+                                            onClick={this.onDeleteClick.bind(
+                                                this,
+                                                group.groupId
+                                            )}
+                                            className="btn btn-danger ml-2 my-1"
+                                        >
+                                            Usuń
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
+}
+
+ActivityGroupList.propTypes = {
+    group: PropTypes.object.isRequired,
+    getGroups: PropTypes.func.isRequired,
+    deleteGroup: PropTypes.func.isRequired
+};
+
+const mapStateToProps = (state) => ({
+    group: state.group
+});
+
+export default connect(mapStateToProps, { getGroups, deleteGroup })(ActivityGroupList);
