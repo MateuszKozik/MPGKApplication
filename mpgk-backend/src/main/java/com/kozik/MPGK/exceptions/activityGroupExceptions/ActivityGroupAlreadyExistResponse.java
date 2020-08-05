6fb14a1261d06d715b55b1a52c16387752eb9cb3
MichalStygar <misiek9753@o2.pl>
2020-08-05 15:55:38 +0200

    Add ActivityGroupAlreadyExistResponse

---
+++ mpgk-backend/src/main/java/com/kozik/MPGK/exceptions/activityGroupExceptions/ActivityGroupAlreadyExistResponse.java
@@ -0,0 +1,11 @@
+package com.kozik.MPGK.exceptions.activityGroupExceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ActivityGroupAlreadyExistResponse {
+
+    public String ActivityGroupAlreadyExist;
+}
