2864caa6e80f8c7dd5fe2958137f442ccb80221b
MichalStygar <misiek9753@o2.pl>
2020-08-05 19:45:39 +0200

    Create AddActivityGroup

---
+++ mpgk-react-frontend/src/components/ActivityGroup/AddActivityGroup.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import classNames from "classnames";
+import { addGroup } from "../../actions/activityGroupActions";
+
+class AddActivityGroup extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            name: "",
+            errors: {}
+        };
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const newGroup = {
+            name: this.state.name
+        };
+        this.props.addGroup(newGroup, this.props.history);
+    }
+
+    static getDerivedStateFromProps(nextProps, prevState) {
+        if (nextProps.errors !== prevState.errors) {
+            return { errors: nextProps.errors };
+        } else {
+            return null;
+        }
+    }
+
+    render() {
+        const { errors } = this.state;
+
+        return (
+            <div className="container mt-2">
+                <form onSubmit={this.onSubmit}>
+                    <div className="row">
+                        <div className="col-md-4 offset-md-4 text-center">
+                            <div className="form-group">
+                                <label>Nazwa</label>
+                                <input
+                                    className={classNames("form-control", {
+                                        "is-invalid": errors.name
+                                    })}
+                                    name="name"
+                                    type="text"
+                                    onChange={this.onChange}
+                                    value={this.state.name}
+                                />
+                                {errors.name && (
+                                    <div className="invalid-feedback">
+                                        {errors.name}
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12 text-center">
+                            <button type="submit" className="btn btn-primary">
+                                Zapisz
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+AddActivityGroup.propTypes = {
+    addGroup: PropTypes.func.isRequired,
+    errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+    errors: state.errors
+});
+
+export default connect(mapStateToProps, { addGroup })(AddActivityGroup);
