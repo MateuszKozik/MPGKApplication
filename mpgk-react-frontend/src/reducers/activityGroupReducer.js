8a754971a778cfe2ef4e7ca8cd099e918aeea87b
MichalStygar <misiek9753@o2.pl>
2020-08-05 19:23:01 +0200

    Add activityGroupReducer

---
+++ mpgk-react-frontend/src/reducers/activityGroupReducer.js
@@ -0,0 +1,33 @@
+import { GET_GROUPS, GET_GROUP, DELETE_GROUP } from "../actions/types";
+
+const initialState = {
+    groups: [],
+    group: {}
+};
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        case GET_GROUPS:
+            return {
+                ...state,
+                groups: action.payload
+            };
+
+        case GET_GROUP:
+            return {
+                ...state,
+                group: action.payload
+            };
+
+        case DELETE_GROUP:
+            return {
+                ...state,
+                groups: state.groups.filter(
+                    (group) => group.groupId !== action.payload
+                )
+            };
+
+        default:
+            return state;
+    }
+}
