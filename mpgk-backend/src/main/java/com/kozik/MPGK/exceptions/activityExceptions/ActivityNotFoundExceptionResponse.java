beb900429244b4803ad64f9a55233ac9cda00995
MichalStygar <misiek9753@o2.pl>
2020-08-05 15:59:10 +0200

    Add ActivityNotFoundExceptionResponse

---
+++ mpgk-backend/src/main/java/com/kozik/MPGK/exceptions/activityExceptions/ActivityNotFoundExceptionResponse.java
@@ -0,0 +1,11 @@
+package com.kozik.MPGK.exceptions.activityExceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ActivityNotFoundExceptionResponse {
+
+    private String ActivityNotFound;
+}
