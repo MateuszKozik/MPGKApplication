471c6dc5c9f55ef310773509f514cbfd7dc642f5
MichalStygar <misiek9753@o2.pl>
2020-08-05 16:10:30 +0200

    Add OverviewAlreadyExistResponse

---
+++ mpgk-backend/src/main/java/com/kozik/MPGK/exceptions/overviewExceptions/OverviewAlreadyExistResponse.java
@@ -0,0 +1,11 @@
+package com.kozik.MPGK.exceptions.overviewExceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class OverviewAlreadyExistResponse {
+
+    public String overviewAlreadyExist;
+}
