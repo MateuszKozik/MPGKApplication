add865fb0283de0fc2f1878f8d4ff1f6c2c556ed
MichalStygar <misiek9753@o2.pl>
2020-08-05 15:51:54 +0200

    Add PersonAlreadyExistResponse

---
+++ mpgk-backend/src/main/java/com/kozik/MPGK/exceptions/personExceptions/PersonAlreadyExistResponse.java
@@ -0,0 +1,12 @@
+package com.kozik.MPGK.exceptions.personExceptions;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PersonAlreadyExistResponse {
+
+    public String PersonAlreadyExist;
+}
