ad46215dd3e0a85777e6b11ac26649fdae1acbb9
MichalStygar <misiek9753@o2.pl>
2020-08-05 16:17:30 +0200

    Change OverviewService

--- mpgk-backend/src/main/java/com/kozik/MPGK/services/OverviewService.java
+++ mpgk-backend/src/main/java/com/kozik/MPGK/services/OverviewService.java
@@ -1,10 +1,11 @@
 package com.kozik.MPGK.services;
 
-import java.util.List;
+
 
 import com.kozik.MPGK.entities.Overview;
 import com.kozik.MPGK.repositories.OverviewRepository;
-
+import com.kozik.MPGK.exceptions.overviewExceptions.OverviewAlreadyExistException;
+import com.kozik.MPGK.exceptions.overviewExceptions.OverviewNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,35 +13,40 @@ import org.springframework.stereotype.Service;
 public class OverviewService {
     @Autowired private OverviewRepository overviewRepository;
 
-    public List<Overview> listAll(){
+    public Iterable<Overview> listAll(){
         return overviewRepository.findAll();
     }
 
-    public void save(Overview overview){
-        overviewRepository.save(overview);
+    public Overview save(Overview overview) {
+        if (overview.getOverviewId() != null) {
+            throw new OverviewAlreadyExistException(overview.getOverviewId());
+        }
+        return overviewRepository.save(overview);
     }
 
-    public Overview get(Long id){
-        return overviewRepository.findById(id).get();
+    public Overview get(Long overviewId) {
+        Overview overview = overviewRepository.findById(overviewId).orElseThrow(() -> new OverviewNotFoundException(overviewId));
+        return overview;
     }
 
-    public void delete(Long id){
-        overviewRepository.deleteById(id);
+    public void delete(Long overviewId) {
+        overviewRepository.delete(get(overviewId));
     }
 
     public Boolean isOverviewExist(Long id){
         return overviewRepository.existsById(id);
     }
 
-    public Overview update(Long id,Overview overview){
-        Overview currnetOverview = get(id);
-        currnetOverview.setStatus(overview.getStatus());
-        currnetOverview.setStartTime(overview.getStartTime());
-        currnetOverview.setEndTime(overview.getEndTime());
-        currnetOverview.setParameter(overview.getParameter());
-        currnetOverview.setComment(overview.getComment());
-        save(currnetOverview);
+    public Overview update(Long overviewId, Overview overview) {
+        Overview newOverview = overviewRepository.findById(overviewId).map(element -> {
+            element.setStatus(overview.getStatus());
+            element.setStartTime(overview.getStartTime());
+            element.setEndTime(overview.getEndTime());
+            element.setParameter(overview.getParameter());
+            element.setComment(overview.getComment());
+            return overviewRepository.save(element);
+        }).orElseThrow(() -> new OverviewNotFoundException(overviewId));
 
-        return currnetOverview;
+        return newOverview;
     }
 }
