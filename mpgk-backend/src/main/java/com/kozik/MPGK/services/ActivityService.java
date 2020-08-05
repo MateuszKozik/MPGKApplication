e428ff2063750495f1f79e72c2cb40b415462b14
MichalStygar <misiek9753@o2.pl>
2020-08-05 14:55:44 +0200

    Add activityExceptions

--- mpgk-backend/src/main/java/com/kozik/MPGK/services/ActivityService.java
+++ mpgk-backend/src/main/java/com/kozik/MPGK/services/ActivityService.java
@@ -1,10 +1,11 @@
 package com.kozik.MPGK.services;
 
-import java.util.List;
+
 
 import com.kozik.MPGK.entities.Activity;
 import com.kozik.MPGK.repositories.ActivityRepository;
-
+import com.kozik.MPGK.exceptions.activityExceptions.ActivityAlreadyExistException;
+import com.kozik.MPGK.exceptions.activityExceptions.ActivityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,34 +13,39 @@ import org.springframework.stereotype.Service;
 public class ActivityService {
     @Autowired private ActivityRepository activityRepository;
 
-    public List<Activity> listAll(){
+    public Iterable<Activity> listAll(){
         return activityRepository.findAll();
     }
 
-    public void save(Activity activity){
-        activityRepository.save(activity);
+    public Activity save(Activity activity) {
+        if (activity.getActivityId() != null) {
+            throw new ActivityAlreadyExistException(activity.getActivityId());
+        }
+        return activityRepository.save(activity);
     }
 
-    public Activity get(Long id){
-        return activityRepository.findById(id).get();
+    public Activity get(Long activityId) {
+        Activity activity = activityRepository.findById(activityId).orElseThrow(() -> new ActivityNotFoundException(activityId));
+        return activity;
     }
 
-    public void delete(Long id){
-        activityRepository.deleteById(id);
+    public void delete(Long activityId) {
+        activityRepository.delete(get(activityId));
     }
 
     public Boolean isActivityExist(Long id){
         return activityRepository.existsById(id);
     }
 
-    public Activity update(Long id, Activity activity){
-        Activity currentActivity = get(id);
-        currentActivity.setName(activity.getName());
-        currentActivity.setType(activity.getType());
-        currentActivity.setEmsr(activity.getEmsr());
-        currentActivity.setSetting(activity.getSetting());
-        save(currentActivity);
-        
-        return currentActivity;
+    public Activity update(Long activityId, Activity activity) {
+        Activity newActivity = activityRepository.findById(activityId).map(element -> {
+            element.setName(activity.getName());
+            element.setType(activity.getType());
+            element.setEmsr(activity.getEmsr());
+            element.setSetting(activity.getSetting());
+            return activityRepository.save(element);
+        }).orElseThrow(() -> new ActivityNotFoundException(activityId));
+
+        return newActivity;
     }
 }
