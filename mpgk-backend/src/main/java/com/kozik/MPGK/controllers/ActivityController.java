e48177f4f7018004af427dc56cb4038c850225fe
MichalStygar <misiek9753@o2.pl>
2020-08-05 15:03:52 +0200

    Change ActivityController

--- mpgk-backend/src/main/java/com/kozik/MPGK/controllers/ActivityController.java
+++ mpgk-backend/src/main/java/com/kozik/MPGK/controllers/ActivityController.java
@@ -1,11 +1,9 @@
 package com.kozik.MPGK.controllers;
 
-import java.util.List;
+
 
 import com.kozik.MPGK.entities.Activity;
 import com.kozik.MPGK.services.ActivityService;
-import com.kozik.MPGK.utilities.ErrorMessage;
-import org.springframework.http.HttpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,68 +15,58 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
+import com.kozik.MPGK.services.MapValidationErrorService;
+import javax.validation.Valid;
+import com.kozik.MPGK.utilities.Message;
+import org.springframework.validation.BindingResult;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/activities")
 public class ActivityController {
 
     @Autowired private ActivityService activityService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     //Get all activities
-    @GetMapping("/activities")
-    public ResponseEntity<List<Activity>> getActivities(){
-        List<Activity> activities = activityService.listAll();
-        if(activities.isEmpty()){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<List<Activity>>(activities, HttpStatus.OK);
+    @GetMapping("")
+    public Iterable<Activity> getActivities() {
+        return activityService.listAll();
     }
 
     //Get single activity
-    @GetMapping("/activities/{activityId}")
-    public ResponseEntity<?> getActivity(@PathVariable("activityId")Long activityId){
-        if(!activityService.isActivityExist(activityId)){
-            return new ResponseEntity<>(new ErrorMessage("Activity with id " + activityId + " not found."),
-            HttpStatus.NOT_FOUND);
-        }
-        Activity activity = activityService.get(activityId);
-        return new ResponseEntity<Activity>(activity, HttpStatus.OK);
+    @GetMapping("/{activityId}")
+    public ResponseEntity<?> getActivity(@PathVariable Long activityId) {
+        return new ResponseEntity<Activity>(activityService.get(activityId), HttpStatus.OK);
     }
 
     //Create activity
-    @PostMapping("/activities")
-    public ResponseEntity<?> createActivity(@RequestBody Activity activity, UriComponentsBuilder builder){
-        Long activityId = activity.getActivityId();
-        if(activityId != null){
-            return new ResponseEntity<>(new ErrorMessage("Unable to create. Activity with id " + activityId 
-            + " already exist."), HttpStatus.CONFLICT);
+    @PostMapping("")
+    public ResponseEntity<?> createActivity(@Valid @RequestBody Activity activity, BindingResult result) {
+        if (result.hasErrors()) {
+            return mapValidationErrorService.MapValidationService(result);
         }
-        activityService.save(activity);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(builder.path("/api/activities/{activityId}").buildAndExpand(activity.getActivityId()).toUri());
-        return new ResponseEntity<String>(headers, HttpStatus.CREATED);
+
+        return new ResponseEntity<Activity>(activityService.save(activity), HttpStatus.CREATED);
     }
 
     //Update activity
-    @PutMapping("/activities/{activityId}")
-    public ResponseEntity<?> updateActivity(@PathVariable("activityId")Long activityId, @RequestBody Activity activity){
-        if(!activityService.isActivityExist(activityId)){
-            return new ResponseEntity<>(new ErrorMessage("Unable to update. Activity with id " + activityId + " not found."),
-            HttpStatus.NOT_FOUND);
+    @PutMapping("/{activityId}")
+    public ResponseEntity<?> updateupdateActivityFluid(@PathVariable Long activityId, @Valid @RequestBody Activity activity,
+            BindingResult result) {
+        if (result.hasErrors()) {
+            return mapValidationErrorService.MapValidationService(result);
         }
-        Activity currentActivity = activityService.update(activityId, activity);      
-        return new ResponseEntity<Activity>(currentActivity, HttpStatus.OK);
+
+        return new ResponseEntity<Activity>(activityService.update(activityId, activity), HttpStatus.OK);
     }
 
     //Delete activity
-    @DeleteMapping("/activities/{activityId}")
-    public ResponseEntity<?> deleteActivity(@PathVariable("activityId")Long activityId){
-        if(!activityService.isActivityExist(activityId)){
-            return new ResponseEntity<>(new ErrorMessage("Unable to delete. Activity with id " + activityId + " not found"),
-            HttpStatus.NOT_FOUND);
-        }
+    @DeleteMapping("/{activityId}")
+    public ResponseEntity<?> deleteActivity(@PathVariable("activityId") Long activityId) {
         activityService.delete(activityId);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<Message>(new Message("Activity with id: " + activityId + " has been removed."),
+                HttpStatus.OK);
     }
 }
