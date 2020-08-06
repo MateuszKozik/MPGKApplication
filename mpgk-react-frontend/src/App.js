2952e9288d9b693519c8fd8d8f1a799ed495c6ff
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-08-06 18:45:12 +0200

    Delete unused files

--- mpgk-react-frontend/src/App.js
+++ mpgk-react-frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Layout/Header";
@@ -28,73 +27,56 @@ import AddOverview from "./components/Overview/AddOverview";
 import UpdateOverview from "./components/Overview/UpdateOverview";
 
 function App() {
-    return (
-        <Provider store={store}>
-            <Router>
-                <div className="App">
-                    <Header />
-                    <Route exact path="/devices" component={DeviceList} />
-                    <Route exact path="/devices/add" component={AddDevice} />
-                    <Route
-                        exact
-                        path="/devices/update/:id"
-                        component={UpdateDevice}
-                    />
-                    <Route exact path="/fluids" component={FluidList} />
-                    <Route exact path="/fluids/add" component={AddFluid} />
-                    <Route
-                        exact
-                        path="/fluids/update/:fluidId"
-                        component={UpdateFluid}
-                    />
-                    <Route
-                        exact
-                        path="/overview-types"
-                        component={OverviewTypeList}
-                    />
-                    <Route
-                        exact
-                        path="/overview-types/add"
-                        component={AddOverviewType}
-                    />
-                    <Route
-                        exact
-                        path="/overview-types/update/:typeId"
-                        component={UpdateOverviewType}
-                    />
-                    <Route exact path="/persons" component={PersonList} />
-                    <Route exact path="/persons/add" component={AddPerson} />
-                    <Route
-                        exact
-                        path="/persons/update/:personId"
-                        component={UpdatePerson}
-                    />
-                    <Route exact path="/groups" component={ActivityGroupList} />
-                    <Route exact path="/groups/add" component={AddActivityGroup} />
-                    <Route
-                        exact
-                        path="/groups/update/:groupId"
-                        component={UpdateActivityGroup}
-                    />
-                    <Route exact path="/activities" component={ActivityList} />
-                    <Route exact path="/activities/add" component={AddActivity} />
-                    <Route
-                        exact
-                        path="/activities/update/:activityId"
-                        component={UpdateActivity}
-                    />
-                    <Route exact path="/overviews" component={OverviewList} />
-                    <Route exact path="/overviews/add" component={AddOverview} />
-                    <Route
-                        exact
-                        path="/overviews/update/:overviewId"
-                        component={UpdateOverview}
-                    />
-                    
-                </div>
-            </Router>
-        </Provider>
-    );
+	return (
+		<Provider store={store}>
+			<Router>
+				<div className="App">
+					<Header />
+					<Route exact path="/devices" component={DeviceList} />
+					<Route exact path="/devices/add" component={AddDevice} />
+					<Route exact path="/devices/update/:id" component={UpdateDevice} />
+					<Route exact path="/fluids" component={FluidList} />
+					<Route exact path="/fluids/add" component={AddFluid} />
+					<Route exact path="/fluids/update/:fluidId" component={UpdateFluid} />
+					<Route exact path="/overview-types" component={OverviewTypeList} />
+					<Route exact path="/overview-types/add" component={AddOverviewType} />
+					<Route
+						exact
+						path="/overview-types/update/:typeId"
+						component={UpdateOverviewType}
+					/>
+					<Route exact path="/persons" component={PersonList} />
+					<Route exact path="/persons/add" component={AddPerson} />
+					<Route
+						exact
+						path="/persons/update/:personId"
+						component={UpdatePerson}
+					/>
+					<Route exact path="/groups" component={ActivityGroupList} />
+					<Route exact path="/groups/add" component={AddActivityGroup} />
+					<Route
+						exact
+						path="/groups/update/:groupId"
+						component={UpdateActivityGroup}
+					/>
+					<Route exact path="/activities" component={ActivityList} />
+					<Route exact path="/activities/add" component={AddActivity} />
+					<Route
+						exact
+						path="/activities/update/:activityId"
+						component={UpdateActivity}
+					/>
+					<Route exact path="/overviews" component={OverviewList} />
+					<Route exact path="/overviews/add" component={AddOverview} />
+					<Route
+						exact
+						path="/overviews/update/:overviewId"
+						component={UpdateOverview}
+					/>
+				</div>
+			</Router>
+		</Provider>
+	);
 }
 
 export default App;
