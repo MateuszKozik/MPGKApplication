0738dbbdd900b67f03d43cc12e4d2ba6da47733e
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-08-07 10:38:15 +0200

    Correct message

--- mpgk-react-frontend/src/actions/activityActions.js
+++ mpgk-react-frontend/src/actions/activityActions.js
@@ -1,66 +1,71 @@
 import axios from "axios";
-import { GET_ERRORS, GET_ACTIVITIES, GET_ACTIVITY, DELETE_ACTIVITY } from "./types";
+import {
+	GET_ERRORS,
+	GET_ACTIVITIES,
+	GET_ACTIVITY,
+	DELETE_ACTIVITY
+} from "./types";
 
 export const addActivity = (activity, history) => async (dispatch) => {
-    try {
-        await axios.post("/api/activities", activity);
-        history.push("/activities");
-        dispatch({
-            type: GET_ERRORS,
-            payload: {}
-        });
-    } catch (error) {
-        dispatch({
-            type: GET_ERRORS,
-            payload: error.response.data
-        });
-    }
+	try {
+		await axios.post("/api/activities", activity);
+		history.push("/activities");
+		dispatch({
+			type: GET_ERRORS,
+			payload: {}
+		});
+	} catch (error) {
+		dispatch({
+			type: GET_ERRORS,
+			payload: error.response.data
+		});
+	}
 };
 
 export const getActivities = () => async (dispatch) => {
-    const res = await axios.get("/api/activities");
-    dispatch({
-        type: GET_ACTIVITIES,
-        payload: res.data
-    });
+	const res = await axios.get("/api/activities");
+	dispatch({
+		type: GET_ACTIVITIES,
+		payload: res.data
+	});
 };
 
 export const getActivity = (activityId, history) => async (dispatch) => {
-    try {
-        const res = await axios.get(`/api/activities/${activityId}`);
-        dispatch({
-            type: GET_ACTIVITY,
-            payload: res.data
-        });
-    } catch (error) {
-        history.push("/activities");
-    }
+	try {
+		const res = await axios.get(`/api/activities/${activityId}`);
+		dispatch({
+			type: GET_ACTIVITY,
+			payload: res.data
+		});
+	} catch (error) {
+		history.push("/activities");
+	}
 };
 
 export const updateActivity = (activityId, updateActivity, history) => async (
-    dispatch
+	dispatch
 ) => {
-    try {
-        await axios.put(`/api/activities/${activityId}`, updateActivity);
-        history.push("/activities");
-        dispatch({
-            type: GET_ERRORS,
-            payload: {}
-        });
-    } catch (error) {
-        dispatch({
-            type: GET_ERRORS,
-            payload: error.response.data
-        });
-    }
+	try {
+		await axios.put(`/api/activities/${activityId}`, updateActivity);
+		history.push("/activities");
+		dispatch({
+			type: GET_ERRORS,
+			payload: {}
+		});
+	} catch (error) {
+		dispatch({
+			type: GET_ERRORS,
+			payload: error.response.data
+		});
+	}
 };
 
 export const deleteActivity = (activityId) => async (dispatch) => {
-    if (window.confirm("Czy jesteś pewny? Spowoduje to usunięcie czynność")) {
-        await axios.delete(`/api/activities/${activityId}`);
-        dispatch({
-            type: DELETE_ACTIVITY,
-            payload: activityId
-        });
-    }
+	if (window.confirm("Czy jesteś pewny? Spowoduje to usunięcie czynności")) {
+		await axios.delete(`/api/activities/${activityId}`);
+		dispatch({
+			type: DELETE_ACTIVITY,
+			payload: activityId
+		});
+	}
 };
