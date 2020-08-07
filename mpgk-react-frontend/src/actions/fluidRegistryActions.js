33b9835837ffa06363689f6308b1844ea082e5d1
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-08-07 21:00:06 +0200

    Correct update function name

--- mpgk-react-frontend/src/actions/fluidRegistryActions.js
+++ mpgk-react-frontend/src/actions/fluidRegistryActions.js
@@ -44,7 +44,7 @@ export const getFluidRegistry = (registryId, history) => async (dispatch) => {
 	}
 };
 
-export const updateFLuidRegistry = (
+export const updateFluidRegistry = (
 	registryId,
 	fluidRegistry,
 	history
