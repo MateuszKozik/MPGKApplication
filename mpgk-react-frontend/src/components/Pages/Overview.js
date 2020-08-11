b26bed3129438af3a7861bfc42b8b669c1e3883f
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-08-11 18:58:43 +0200

    Type of input field is generated automatically

--- mpgk-react-frontend/src/components/Pages/Overview.js
+++ mpgk-react-frontend/src/components/Pages/Overview.js
@@ -9,40 +9,62 @@ class Overview extends Component {
 		this.props.getOverviewsByConnection(connectionId, this.props.history);
 	}
 
+	selectInput(type) {
+		switch (type) {
+			case "Pole tekstowe":
+				return <input type="text" className="form-control" name="parametr" />;
+			case "Zaznaczenie":
+				return (
+					<input type="checkbox" className="form-control" name="parametr" />
+				);
+			case "Zakres liczb":
+				return <input type="number" className="form-control" name="parametr" />;
+			case "Pole wyboru":
+				return (
+					<div className="form-check">
+						<label>TAK</label>
+						<input type="radio" name="parametr" value="TAK" />
+						<label>NIE</label>
+						<input type="radio" name="parametr" value="NIE" />
+					</div>
+				);
+			default:
+				return <input type="text" className="form-control" name="comment" />;
+		}
+	}
+
 	render() {
 		const { overviewsList } = this.props.overview;
 
 		return (
 			<div className="container mt-2">
-				{overviewsList.map((overviewList) => (
-					<div>
+				{overviewsList.map((overviewList, k) => (
+					<div key={k}>
 						<h4>{overviewList.activityGroup.name}</h4>
 						<div className="table-responsive mt-2">
 							<table className="table ">
 								<thead>
 									<tr>
 										<th>Zadanie</th>
-										<th>Patametr</th>
+										<th>Parametr</th>
 										<th>Komentarz</th>
+										<th>Zapisz</th>
 									</tr>
 								</thead>
 								<tbody>
 									{overviewList.overviews.map((overview, i) => (
 										<tr key={i}>
 											<td>{overview.activity.name}</td>
-											<td>
-												<input
-													type="text"
-													className="form-control"
-													name="parametr"
-												></input>
-											</td>
+											<td>{this.selectInput(overview.activity.type)}</td>
 											<td>
 												<input
 													type="text"
 													className="form-control"
 													name="comment"
-												></input>
+												/>
+											</td>
+											<td>
+												<button className="btn btn-primary">Zapisz</button>
 											</td>
 										</tr>
 									))}
