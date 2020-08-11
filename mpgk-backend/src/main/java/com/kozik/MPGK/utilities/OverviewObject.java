acf600f98138e3fcad63df984ff67cc1e68d344d
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-08-11 12:33:09 +0200

    Create class to store activities groups and overviews

---
+++ mpgk-backend/src/main/java/com/kozik/MPGK/utilities/OverviewObject.java
@@ -0,0 +1,17 @@
+package com.kozik.MPGK.utilities;
+
+import java.util.List;
+
+import com.kozik.MPGK.entities.ActivityGroup;
+import com.kozik.MPGK.entities.Overview;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class OverviewObject {
+
+    private ActivityGroup activityGroup;
+    private List<Overview> overviews;
+}
