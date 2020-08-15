c28d25e0143c87a57873d47766888f347714c506
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-08-15 18:00:24 +0200

    Create find and count functions

--- mpgk-backend/src/main/java/com/kozik/MPGK/repositories/ConnectionRepository.java
+++ mpgk-backend/src/main/java/com/kozik/MPGK/repositories/ConnectionRepository.java
@@ -1,8 +1,10 @@
 package com.kozik.MPGK.repositories;
 
+import java.util.List;
+
 import com.kozik.MPGK.entities.Connection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ConnectionRepository extends JpaRepository<Connection, Long>{
-    
+public interface ConnectionRepository extends JpaRepository<Connection, Long> {
+    List<Connection> findByOverviewTypeNameNotLikeAndDeviceStatus(String overviewType, Boolean status);
 }
--- mpgk-backend/src/main/java/com/kozik/MPGK/repositories/OverviewRepository.java
+++ mpgk-backend/src/main/java/com/kozik/MPGK/repositories/OverviewRepository.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import com.kozik.MPGK.entities.ActivityGroup;
+import com.kozik.MPGK.entities.Connection;
 import com.kozik.MPGK.entities.Overview;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,5 +12,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface OverviewRepository extends JpaRepository<Overview, Long> {
 
     List<Overview> findByActivityActivityGroupAndEndTimeGreaterThan(ActivityGroup activityGroup, LocalDateTime endTime);
+
     List<Overview> findByActivityNameAndParameter(String name, String parameter);
+
+    Integer countByActivityActivityGroupConnectionAndStatus(Connection connection, String status);
 }
