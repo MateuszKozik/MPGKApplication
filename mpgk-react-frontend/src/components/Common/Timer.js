58a37b8c16ee48f520d3ea97da0d7b2fc3caddbc
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-08-18 18:12:06 +0200

    Timer will display minutes and seconds

--- mpgk-react-frontend/src/components/Common/Timer.js
+++ mpgk-react-frontend/src/components/Common/Timer.js
@@ -1,9 +1,38 @@
 import React from "react";
 import Countdown from "react-countdown";
 
-const renderer = ({ days, hours }) => {
+const renderer = ({ days, hours, minutes, seconds }) => {
 	if (days === 0) {
-		if (hours === 1) {
+		if (hours === 0) {
+			if (minutes === 0) {
+				if (seconds === 1) {
+					return <span>{seconds} sekunda </span>;
+				} else if (
+					seconds <= 4 ||
+					(seconds >= 22 && seconds <= 24) ||
+					(seconds >= 32 && seconds <= 34) ||
+					(seconds >= 42 && seconds <= 44) ||
+					(seconds >= 52 && seconds <= 54)
+				) {
+					return <span>{seconds} sekundy </span>;
+				} else {
+					return <span>{seconds} sekund </span>;
+				}
+			}
+			if (minutes === 1) {
+				return <span>{minutes} minuta </span>;
+			} else if (
+				minutes <= 4 ||
+				(minutes >= 22 && minutes <= 24) ||
+				(minutes >= 32 && minutes <= 34) ||
+				(minutes >= 42 && minutes <= 44) ||
+				(minutes >= 52 && minutes <= 54)
+			) {
+				return <span>{minutes} minuty </span>;
+			} else {
+				return <span>{minutes} minut </span>;
+			}
+		} else if (hours === 1) {
 			return <span>{hours} godzina</span>;
 		} else if (hours <= 4 || hours >= 22) {
 			return <span>{hours} godziny</span>;
