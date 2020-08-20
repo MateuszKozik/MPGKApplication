38415b87c32f7c239a4111b76648df9a90db4f87
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-08-20 16:08:51 +0200

    Add find activities by activity group methods

--- mpgk-backend/src/main/java/com/kozik/MPGK/repositories/ActivityRepository.java
+++ mpgk-backend/src/main/java/com/kozik/MPGK/repositories/ActivityRepository.java
@@ -1,10 +1,13 @@
 package com.kozik.MPGK.repositories;
 
+import java.util.List;
+
 import com.kozik.MPGK.entities.Activity;
+import com.kozik.MPGK.entities.ActivityGroup;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
 
-    
+    List<Activity> findByActivityGroup(ActivityGroup activityGroup);
 }
