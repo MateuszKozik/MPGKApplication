e1eebd1786926c1786333447f2de23c03bd10163
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-08-29 17:00:42 +0200

    Correct fluid actions to use an update and add types

--- mpgk-react-frontend/src/actions/fluidActions.js
+++ mpgk-react-frontend/src/actions/fluidActions.js
@@ -1,66 +1,79 @@
 import axios from "axios";
-import { GET_ERRORS, GET_FLUIDS, GET_FLUID, DELETE_FLUID } from "./types";
+import {
+	GET_ERRORS,
+	GET_FLUIDS,
+	GET_FLUID,
+	DELETE_FLUID,
+	ADD_FLUID,
+	UPDATE_FLUID
+} from "./types";
 
-export const addFluid = (fluid, history) => async (dispatch) => {
-    try {
-        await axios.post("/api/fluids", fluid);
-        history.push("/fluids");
-        dispatch({
-            type: GET_ERRORS,
-            payload: {}
-        });
-    } catch (error) {
-        dispatch({
-            type: GET_ERRORS,
-            payload: error.response.data
-        });
-    }
+export const addFluid = (fluid) => async (dispatch) => {
+	try {
+		const res = await axios.post("/api/fluids", fluid);
+		dispatch({
+			type: ADD_FLUID,
+			payload: res.date
+		});
+		dispatch({
+			type: GET_ERRORS,
+			payload: {}
+		});
+		return res;
+	} catch (error) {
+		dispatch({
+			type: GET_ERRORS,
+			payload: error.response.data
+		});
+	}
 };
 
 export const getFluids = () => async (dispatch) => {
-    const res = await axios.get("/api/fluids");
-    dispatch({
-        type: GET_FLUIDS,
-        payload: res.data
-    });
+	const res = await axios.get("/api/fluids");
+	dispatch({
+		type: GET_FLUIDS,
+		payload: res.data
+	});
 };
 
 export const getFluid = (fluidId, history) => async (dispatch) => {
-    try {
-        const res = await axios.get(`/api/fluids/${fluidId}`);
-        dispatch({
-            type: GET_FLUID,
-            payload: res.data
-        });
-    } catch (error) {
-        history.push("/fluids");
-    }
+	try {
+		const res = await axios.get(`/api/fluids/${fluidId}`);
+		dispatch({
+			type: GET_FLUID,
+			payload: res.data
+		});
+	} catch (error) {
+		history.push("/fluids");
+	}
 };
 
-export const updateFluid = (fluidId, updateFluid, history) => async (
-    dispatch
-) => {
-    try {
-        await axios.put(`/api/fluids/${fluidId}`, updateFluid);
-        history.push("/fluids");
-        dispatch({
-            type: GET_ERRORS,
-            payload: {}
-        });
-    } catch (error) {
-        dispatch({
-            type: GET_ERRORS,
-            payload: error.response.data
-        });
-    }
+export const updateFluid = (fluidId, updateFluid) => async (dispatch) => {
+	try {
+		const res = await axios.put(`/api/fluids/${fluidId}`, updateFluid);
+		dispatch({
+			type: UPDATE_FLUID,
+			payload: res.data
+		});
+		dispatch({
+			type: GET_ERRORS,
+			payload: {}
+		});
+		return res;
+	} catch (error) {
+		dispatch({
+			type: GET_ERRORS,
+			payload: error.response.data
+		});
+	}
 };
 
 export const deleteFluid = (fluidId) => async (dispatch) => {
-    if (window.confirm("Czy jesteś pewny? Spowoduje to usunięcie płynu")) {
-        await axios.delete(`/api/fluids/${fluidId}`);
-        dispatch({
-            type: DELETE_FLUID,
-            payload: fluidId
-        });
-    }
+	if (window.confirm("Czy jesteś pewny? Spowoduje to usunięcie płynu")) {
+		await axios.delete(`/api/fluids/${fluidId}`);
+		dispatch({
+			type: DELETE_FLUID,
+			payload: fluidId
+		});
+	}
 };
