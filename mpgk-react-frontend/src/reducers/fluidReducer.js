90acf909313e6f75a971be9062ee1e374633ae85
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-08-29 17:01:42 +0200

    Update fluid reducer

--- mpgk-react-frontend/src/reducers/fluidReducer.js
+++ mpgk-react-frontend/src/reducers/fluidReducer.js
@@ -1,33 +1,58 @@
-import { GET_FLUID, GET_FLUIDS, DELETE_FLUID } from "../actions/types";
+import {
+	GET_FLUID,
+	GET_FLUIDS,
+	DELETE_FLUID,
+	UPDATE_FLUID,
+	ADD_FLUID
+} from "../actions/types";
 
 const initialState = {
-    fluids: [],
-    fluid: {}
+	fluids: [],
+	fluid: {}
 };
 
 export default function (state = initialState, action) {
-    switch (action.type) {
-        case GET_FLUIDS:
-            return {
-                ...state,
-                fluids: action.payload
-            };
+	switch (action.type) {
+		case GET_FLUIDS:
+			return {
+				...state,
+				fluids: action.payload
+			};
 
-        case GET_FLUID:
-            return {
-                ...state,
-                fluid: action.payload
-            };
+		case GET_FLUID:
+			return {
+				...state,
+				fluid: action.payload
+			};
 
-        case DELETE_FLUID:
-            return {
-                ...state,
-                fluids: state.fluids.filter(
-                    (fluid) => fluid.fluidId !== action.payload
-                )
-            };
+		case UPDATE_FLUID:
+			return {
+				...state,
+				fluids: state.fluids.map((fluid) => {
+					if (fluid.fluidId === action.payload.fluidId) {
+						return {
+							...fluid,
+							...action.payload
+						};
+					} else {
+						return fluid;
+					}
+				})
+			};
 
-        default:
-            return state;
-    }
+		case ADD_FLUID:
+			return {
+				...state,
+				fluids: [action.payload, ...state.fluids]
+			};
+
+		case DELETE_FLUID:
+			return {
+				...state,
+				fluids: state.fluids.filter((fluid) => fluid.fluidId !== action.payload)
+			};
+
+		default:
+			return state;
+	}
 }
