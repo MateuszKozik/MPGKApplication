76f1914bf763462a3dd0f580c754766cbda69eb6
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-08-30 11:07:45 +0200

    Create table styles

---
+++ mpgk-react-frontend/src/consts/themeConsts.js
@@ -0,0 +1,31 @@
+export const tableStyles = (theme) => ({
+	head: {
+		backgroundColor: "#017bff",
+		color: "#fff"
+	},
+	form: {
+		padding: "40px 10px 10px 10px",
+		textAlign: "center"
+	},
+	fab: {
+		position: "fixed",
+		bottom: "40px",
+		right: "40px"
+	},
+	container: {
+		marginTop: "20px"
+	},
+	title: {
+		[theme.breakpoints.down("md")]: {
+			fontSize: "35px"
+		},
+		padding: "5px",
+		textAlign: "center"
+	},
+	search: {
+		[theme.breakpoints.down("md")]: {
+			textAlign: "center"
+		},
+		padding: "10px"
+	}
+});
