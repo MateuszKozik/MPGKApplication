cab09bbbd3c8bfafc86c02e0570a459bcc96f5be
MichalStygar <misiek9753@o2.pl>
2020-08-31 17:28:53 +0200

    Create activityList

--- mpgk-react-frontend/src/components/Activity/ActivityList.js
+++ mpgk-react-frontend/src/components/Activity/ActivityList.js
@@ -1,76 +1,311 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { getActivities, deleteActivity } from "../../actions/activityActions";
+import {
+	getActivities,
+	updateActivity,
+	clearActivityState
+} from "../../actions/activityActions";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
-import AddButton from "../Common/AddButton";
+import {
+	Grid,
+	Typography,
+	IconButton,
+	Button,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+	Dialog,
+	LinearProgress,
+	TextField,
+	InputAdornment,
+	Tooltip
+} from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import EditIcon from "@material-ui/icons/Edit";
+import SearchIcon from "@material-ui/icons/Search";
+import { FormikTextField } from "formik-material-fields";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import { setSnackbar } from "../../reducers/snackbarReducer";
+import { tableStyles } from "./../../consts/themeConsts";
+
+const validationSchema = Yup.object().shape({
+	name: Yup.string()
+        .required("Nazwa czynności jest wymagana"),
+    emsr: Yup.string()
+        .max(100, "Wprowadź któtszy EMSR"),
+    setting: Yup.string()
+        .max(100, "Wprowadź któtszą nastawę")
+		
+});
 
 class ActivityList extends Component {
+	state = {
+		dialogOpen: false,
+		activityId: "",
+        name: "",
+        emsr: "",
+        setting: "",
+        type: "",
+        groupId: "",
+		actionType: "",
+		search: "",
+		errors: {}
+	};
+
+	updateSearch = (event) => {
+		this.setState({ search: event.target.value });
+	};
+
+	onChange = (e) => {
+		this.setState({ [e.target.name]: e.target.value });
+    };
+    
+    
+
+	componentWillUnmount() {
+		this.props.clearActivityState();
+	}
+
+	handleOpen = (activityId, name, emsr, setting, type, groupId, listItems, actionType = "edit") => {
+		this.setState({
+			dialogOpen: true,
+            actionType: actionType,
+            name: name,
+            emsr: emsr,
+            setting: setting,
+            type: type,
+            listItems: listItems,
+            groupId: groupId,
+            activityId: activityId
+		});
+	};
+
+	handleClose = () => {
+		this.setState({
+			dialogOpen: false,
+            name: "",
+            emsr: "",
+            setting: "",
+            type: "",
+            listItems: "",
+            groupId: "",
+			activityId: "",
+			actionType: ""
+		});
+	};
+
+	onSubmit = (values, { setSubmitting }) => {
+		setTimeout(() => {
+			setSubmitting(false);
+			if (this.state.actionType === "edit") {
+				const updatedActivity = {
+					activityId: this.state.activityId,
+                    name: values.name,
+                    emsr: values.emsr,
+                    setting: values.setting,
+                    type: this.state.type,
+                    listItems:this.state.listItems,
+                    activityGroup: { groupId: this.state.groupId }
+                };
+                
+
+				this.props.updateActivity(this.state.activityId, updatedActivity).then((res) => {
+					if (res) {
+						this.props.setSnackbar(true, "Dane czynności zaktualizowane!");
+						this.handleClose();
+					} else {
+						this.props.setSnackbar(true, "Wystąpił błąd!");
+					}
+				});
+			} 
+		}, 500);
+	};
+
+	static getDerivedStateFromProps(nextProps, prevState) {
+		if (nextProps.errors !== prevState.errors) {
+			return { errors: nextProps.errors };
+		} else {
+			return null;
+		}
+	}
+
 	componentDidMount() {
 		this.props.getActivities();
 	}
 
-	onDeleteClick = (activityId) => {
-		this.props.deleteActivity(activityId);
-	};
-
 	render() {
+		const { classes } = this.props;
 		const { activities } = this.props.activity;
+		const { errors } = this.props;
+		const filtered = activities.filter((activity) => {
+			return activity.name.toLowerCase().includes(this.state.search.toLowerCase());
+		});
+
 		return (
-			<div className="container mt-2">
-				<h1 className="display-4 text-center mt-2">Czynności</h1>
-				<div className="row">
-					<div className="col-md-4 my-1">
-						<AddButton
-							link="/activities/add"
-							className="btn btn-info"
-							message="Dodaj czynność"
+			<>
+				<Grid container className={classes.container}>
+					<Grid item xs={12}>
+						<Typography variant="h2" className={classes.title}>
+							Czynności
+						</Typography>
+					</Grid>
+					<Grid item xs={false} md={2} />
+					<Grid item xs={12} md={8} className={classes.search}>
+						<TextField
+							InputProps={{
+								startAdornment: (
+									<InputAdornment position="start">
+										<SearchIcon />
+									</InputAdornment>
+								)
+							}}
+							type="search"
+							placeholder="Szukaj..."
+							onChange={(e) => this.updateSearch(e)}
 						/>
-					</div>
-				</div>
-				<div className="table-responsive mt-2">
-					<table className="table ">
-						<thead>
-							<tr>
-								<th>Nazwa</th>
-								<th>Typ pola</th>
-								<th>EMSR</th>
-								<th>Nastawa</th>
-								<th>Elementy listy</th>
-								<th>Akcje</th>
-							</tr>
-						</thead>
-						<tbody>
-							{activities.map((activity) => (
-								<tr key={activity.activityId}>
-									<td>{activity.name}</td>
-									<td>{activity.type}</td>
-									<td>{activity.emsr}</td>
-									<td>{activity.setting}</td>
-									<td>{activity.listItems}</td>
-									<td>
-										<Link
-											to={`/activities/update/${activity.activityId}`}
-											className="btn btn-primary my-1"
+					</Grid>
+					<Grid item xs={false} md={2} />
+					<Grid item xs={false} md={2} />
+					<Grid item xs={12} md={8}>
+						<TableContainer>
+							<Table>
+								<TableHead>
+									<TableRow>
+										<TableCell className={classes.head}>
+											<Typography>Nazwa</Typography>
+										</TableCell>
+                                        <TableCell className={classes.head}>
+											<Typography>EMSR</Typography>
+										</TableCell>
+                                        <TableCell className={classes.head}>
+											<Typography>Nastawa</Typography>
+										</TableCell>
+                                        <TableCell className={classes.head}>
+											<Typography>Elementy listy</Typography>
+										</TableCell>
+										<TableCell className={classes.head}>
+											<Typography>Akcje</Typography>
+										</TableCell>
+									</TableRow>
+								</TableHead>
+								<TableBody>
+									{filtered.map((activity,index) => (
+										<TableRow key={index}>
+											<TableCell>
+												<Typography>{activity.name}</Typography>
+											</TableCell>
+                                            <TableCell>
+												<Typography>{activity.emsr}</Typography>
+											</TableCell>
+                                            <TableCell>
+												<Typography>{activity.setting}</Typography>
+											</TableCell>
+                                            <TableCell>
+                                                 <Typography>{activity.listItems}</Typography>
+											</TableCell>
+											<TableCell>
+												<Tooltip title="Edytuj">
+													<IconButton
+														color="primary"
+														onClick={() =>
+                                                            this.handleOpen(activity.activityId, activity.name, activity.emsr, activity.setting, activity.type,activity.activityGroup.groupId,activity.listItems, "edit")
+                                                            
+                                                        }
+                                                       
+													>
+														<EditIcon />
+													</IconButton>
+												</Tooltip>
+											
+											</TableCell>
+										</TableRow>
+									))}
+								</TableBody>
+							</Table>
+						</TableContainer>
+					</Grid>
+					<Grid item xs={false} md={2} />
+				</Grid>
+
+				<Dialog open={this.state.dialogOpen} onClose={this.handleClose}>
+					<Formik
+						initialValues={{
+                            name: this.state.name,
+                            emsr: this.state.emsr,
+                            setting: this.state.setting
+						}}
+						validationSchema={validationSchema}
+						onSubmit={(values, { setSubmitting }) =>
+							this.onSubmit(values, { setSubmitting })
+						}
+					>
+						{({ isSubmitting }) => (
+							<Form className={classes.form}>
+								<Grid container spacing={2} justify="center">
+									<Grid item xs={12}>
+										<FormikTextField
+											error={errors.name}
+											id="name"
+											name="name"
+											label="Nazwa"
+											variant="outlined"
+											required
+											helperText={errors.name}
+											onChange={this.onChange}
+										/>
+									</Grid>
+                                    <Grid item xs={12}>
+										<FormikTextField
+											error={errors.emsr}
+											id="emsr"
+											name="emsr"
+											label="EMSR"
+											variant="outlined"
+											helperText={errors.emsr}
+											onChange={this.onChange}
+										/>
+									</Grid>
+                                    <Grid item xs={12}>
+										<FormikTextField
+											error={errors.setting}
+											id="setting"
+											name="setting"
+											label="Nastawa"
+											variant="outlined"
+											helperText={errors.setting}
+											onChange={this.onChange}
+										/>
+									</Grid>
+									<Grid item xs={3} />
+									<Grid item xs={3}>
+										<Button onClick={this.handleClose} color="primary">
+											Anuluj
+										</Button>
+									</Grid>
+									<Grid item xs={3}>
+										<Button
+											type="submit"
+											color="primary"
+											disabled={isSubmitting}
 										>
-											Edytuj
-										</Link>
-										<button
-											onClick={this.onDeleteClick.bind(
-												this,
-												activity.activityId
-											)}
-											className="btn btn-danger ml-2 my-1"
-										>
-											Usuń
-										</button>
-									</td>
-								</tr>
-							))}
-						</tbody>
-					</table>
-				</div>
-			</div>
+											Zapisz
+										</Button>
+									</Grid>
+									<Grid item xs={3} />
+									<Grid item xs={12}>
+										{isSubmitting && <LinearProgress />}
+									</Grid>
+								</Grid>
+							</Form>
+						)}
+					</Formik>
+				</Dialog>
+				
+			</>
 		);
 	}
 }
@@ -78,13 +313,39 @@ class ActivityList extends Component {
 ActivityList.propTypes = {
 	activity: PropTypes.object.isRequired,
 	getActivities: PropTypes.func.isRequired,
-	deleteActivity: PropTypes.func.isRequired
+	updateActivity: PropTypes.func.isRequired,
+	clearActivityState: PropTypes.func.isRequired,
+	setSnackbar: PropTypes.func.isRequired
 };
 
-const mapStateToProps = (state) => ({
-	activity: state.activity
-});
+const mapStateToProps = (state) => {
+	return {
+		activity: state.activity,
+		errors: state.errors
+	};
+};
 
-export default connect(mapStateToProps, { getActivities, deleteActivity })(
-	ActivityList
-);
+const mapDispatchToProps = (dispatch) => {
+	return {
+		getActivities: () => {
+			dispatch(getActivities());
+		},
+		clearActivityState: () => {
+			dispatch(clearActivityState());
+		},
+		setSnackbar: (snackbarOpen, snackbarMessage, snackbarTime) => {
+			dispatch(setSnackbar(snackbarOpen, snackbarMessage, snackbarTime));
+		},
+		updateActivity(activityId, updatedActivity) {
+			return dispatch(updateActivity(activityId, updatedActivity)).then((res) => {
+				if (res && res.status === 200) return res;
+			});
+		},
+		
+	};
+};
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(withStyles(tableStyles)(ActivityList));
