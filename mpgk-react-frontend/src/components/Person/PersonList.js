e2131e953dae79a991af83ed556de7f7fbaae0ad
MichalStygar <misiek9753@o2.pl>
2020-08-31 13:10:57 +0200

    Change person list

--- mpgk-react-frontend/src/components/Person/PersonList.js
+++ mpgk-react-frontend/src/components/Person/PersonList.js
@@ -1,67 +1,318 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { getPersons, deletePerson } from "../../actions/personActions";
+import {
+	getPersons,
+	deletePerson,
+	addPerson,
+    updatePerson,
+	clearPersonState
+} from "../../actions/personActions";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
-import AddButton from "../Common/AddButton";
+import { withStyles } from "@material-ui/core";
+import { tableStyles } from "../../consts/themeConsts";
+import {
+	Grid,
+	Typography,
+	IconButton,
+	Fab,
+	Button,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+	Dialog,
+	LinearProgress,
+	TextField,
+	InputAdornment,
+	Tooltip
+} from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import EditIcon from "@material-ui/icons/Edit";
+import SearchIcon from "@material-ui/icons/Search";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { FormikTextField, FormikSwitchField } from "formik-material-fields";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import { setSnackbar } from "../../reducers/snackbarReducer";
+
+const validationSchema = Yup.object().shape({
+	name: Yup.string()
+		.required("Imię jest wymagane")
+        .max(250, "Wprowadź któtszą nazwę"),
+    surname: Yup.string()
+		.required("Nazwisko jest wymagane")
+		.max(250, "Wprowadź któtszą nazwę")
+});
 
 class PersonList extends Component {
+	state = {
+		dialogOpen: false,
+		personId: "",
+		name: "",
+        surname: "",
+        actionType: "",
+		search: "",
+		errors: {}
+	};
+
 	componentDidMount() {
 		this.props.getPersons();
 	}
+
+	updateSearch = (event) => {
+		this.setState({ search: event.target.value });
+	};
+
+	onChange = (e) => {
+		this.setState({ [e.target.name]: e.target.value });
+	};
 
 	onDeleteClick = (personId) => {
 		this.props.deletePerson(personId);
 	};
 
+	componentWillUnmount() {
+		this.props.clearPersonState();
+	}
+
+	handleOpen = (personId, name, surname, actionType = "add") => {
+		this.setState({
+			dialogOpen: true,
+			actionType: actionType
+		});
+		if (actionType === "edit") {
+			this.setState({
+				personId: personId,
+				name: name,
+				surname: surname
+			});
+		}
+	};
+
+	handleClose = () => {
+		this.setState({
+			dialogOpen: false,
+			personId: "",
+			name: "",
+			surname: "",
+			actionType: ""
+		});
+	};
+
+	onSubmit = (values, { setSubmitting }) => {
+		setTimeout(() => {
+			setSubmitting(false);
+			if (this.state.actionType === "add") {
+				const newPerson = { name: values.name, surname: values.surname };
+
+				this.props.addPerson(newPerson).then((res) => {
+					if (res) {
+						this.props.setSnackbar(true, "Dodano nową osobę!");
+						this.handleClose();
+					} else {
+						this.props.setSnackbar(true, "Wystąpił błąd!");
+					}
+				});
+			} else {
+				const updatedPerson = {
+					personId: this.state.personId,
+					name: values.name,
+					surname: values.surname
+				};
+
+				this.props
+					.updatePerson(this.state.personId, updatedPerson)
+					.then((res) => {
+						if (res) {
+							this.props.setSnackbar(true, "Dane osoby zaktualizowane!");
+							this.handleClose();
+						} else {
+							this.props.setSnackbar(true, "Wystąpił błąd!");
+						}
+					});
+			}
+		}, 500);
+	};
+
+	static getDerivedStateFromProps(nextProps, prevState) {
+		if (nextProps.errors !== prevState.errors) {
+			return { errors: nextProps.errors };
+		} else {
+			return null;
+		}
+	}
+
 	render() {
 		const { persons } = this.props.person;
+		const { classes } = this.props;
+		const { errors } = this.props;
+		const filtered = persons.filter((person) => {
+			return person.name
+				.toLowerCase()
+				.includes(this.state.search.toLowerCase());
+		});
+
 		return (
-			<div className="container mt-2">
-				<h1 className="display-4 text-center mt-2">Osoby</h1>
-				<div className="row">
-					<div className="col-md-4 my-1">
-						<AddButton
-							link="/persons/add"
-							className="btn btn-info"
-							message="Dodaj osobę"
+			<>
+				<Grid container className={classes.container}>
+					<Grid item xs={12}>
+						<Typography variant="h2" className={classes.title}>
+							Osoby
+						</Typography>
+					</Grid>
+					<Grid item xs={false} md={2} />
+					<Grid item xs={12} md={8} className={classes.search}>
+						<TextField
+							InputProps={{
+								startAdornment: (
+									<InputAdornment position="start">
+										<SearchIcon />
+									</InputAdornment>
+								)
+							}}
+							type="search"
+							placeholder="Szukaj..."
+							onChange={(e) => this.updateSearch(e)}
 						/>
-					</div>
-				</div>
-				<div className="table-responsive mt-2">
-					<table className="table ">
-						<thead>
-							<tr>
-								<th>Imię</th>
-								<th>Nazwisko</th>
-								<th>Akcje</th>
-							</tr>
-						</thead>
-						<tbody>
-							{persons.map((person) => (
-								<tr key={person.personId}>
-									<td>{person.name}</td>
-									<td>{person.surname}</td>
-									<td>
-										<Link
-											to={`/persons/update/${person.personId}`}
-											className="btn btn-primary my-1"
+					</Grid>
+					<Grid item xs={false} md={2} />
+					<Grid item xs={false} md={2} />
+					<Grid item xs={12} md={8}>
+						<TableContainer>
+							<Table>
+								<TableHead>
+									<TableRow>
+										<TableCell className={classes.head}>
+											<Typography>Imię</Typography>
+										</TableCell>
+										<TableCell className={classes.head}>
+											<Typography>Nazwisko</Typography>
+										</TableCell>
+										<TableCell className={classes.head}>
+											<Typography>Akcje</Typography>
+										</TableCell>
+									</TableRow>
+								</TableHead>
+								<TableBody>
+									{filtered.map((person) => {
+										return (
+											<TableRow key={person.personId}>
+												<TableCell>
+													<Typography>{person.name}</Typography>
+												</TableCell>
+												<TableCell>
+                                                        <Typography>{person.surname}</Typography>	
+												</TableCell>
+												<TableCell>
+													<Tooltip title="Edytuj">
+														<IconButton
+															color="primary"
+															onClick={() =>
+																this.handleOpen(
+																	person.personId,
+																	person.name,
+																	person.surname,
+																	"edit"
+																)
+															}
+														>
+															<EditIcon />
+														</IconButton>
+													</Tooltip>
+													<Tooltip title="Usuń">
+														<IconButton
+															onClick={() =>
+																this.props.deletePerson(person.personId)
+															}
+														>
+															<DeleteIcon />
+														</IconButton>
+													</Tooltip>
+												</TableCell>
+											</TableRow>
+										);
+									})}
+								</TableBody>
+							</Table>
+						</TableContainer>
+					</Grid>
+					<Grid item xs={false} md={2} />
+				</Grid>
+
+				<Dialog open={this.state.dialogOpen} onClose={this.handleClose}>
+					<Formik
+						initialValues={{
+							name: this.state.name,
+							surname: this.state.surname,
+						}}
+						validationSchema={validationSchema}
+						onSubmit={(values, { setSubmitting }) =>
+							this.onSubmit(values, { setSubmitting })
+						}
+					>
+						{({ isSubmitting, values }) => (
+							<Form className={classes.form}>
+								<Grid container spacing={2} justify="center">
+									<Grid item xs={12}>
+										<FormikTextField
+											error={errors.name}
+											id="name"
+											name="name"
+											label="Imię"
+											variant="outlined"
+											required
+											helperText={errors.name}
+											onChange={this.onChange}
+										/>
+									</Grid>
+									<Grid item xs={12}>
+                                    <FormikTextField
+											error={errors.surname}
+											id="surname"
+											name="surname"
+											label="Nazwisko"
+											variant="outlined"
+											required
+											helperText={errors.surname}
+											onChange={this.onChange}
+										/>
+									</Grid>
+									<Grid item xs={3} />
+									<Grid item xs={3}>
+										<Button onClick={this.handleClose} color="primary">
+											Anuluj
+										</Button>
+									</Grid>
+									<Grid item xs={3}>
+										<Button
+											type="submit"
+											color="primary"
+											disabled={isSubmitting}
 										>
-											Edytuj
-										</Link>
-										<button
-											onClick={this.onDeleteClick.bind(this, person.personId)}
-											className="btn btn-danger ml-2 my-1"
-										>
-											Usuń
-										</button>
-									</td>
-								</tr>
-							))}
-						</tbody>
-					</table>
-				</div>
-			</div>
+											Zapisz
+										</Button>
+									</Grid>
+									<Grid item xs={3} />
+									<Grid item xs={12}>
+										{isSubmitting && <LinearProgress />}
+									</Grid>
+								</Grid>
+							</Form>
+						)}
+					</Formik>
+				</Dialog>
+				<Tooltip title="Dodaj">
+					<Fab
+						className={classes.fab}
+						color="secondary"
+						onClick={this.handleOpen}
+					>
+						<AddIcon fontSize="large" />
+					</Fab>
+				</Tooltip>
+			</>
 		);
 	}
 }
@@ -69,13 +320,46 @@ class PersonList extends Component {
 PersonList.propTypes = {
 	person: PropTypes.object.isRequired,
 	getPersons: PropTypes.func.isRequired,
-	deletePerson: PropTypes.func.isRequired
+	deletePerson: PropTypes.func.isRequired,
+	addPerson: PropTypes.func.isRequired,
+	updatePerson: PropTypes.func.isRequired,
+	clearPersonState: PropTypes.func.isRequired,
+	setSnackbar: PropTypes.func.isRequired
 };
 
 const mapStateToProps = (state) => ({
-	person: state.person
+	person: state.person,
+	errors: state.errors
 });
 
-export default connect(mapStateToProps, { getPersons, deletePerson })(
-	PersonList
-);
+const mapDispatchToProps = (dispatch) => {
+	return {
+		getPersons: () => {
+			dispatch(getPersons());
+		},
+		deletePerson: (personId) => {
+			dispatch(deletePerson(personId));
+		},
+		clearPersonState: () => {
+			dispatch(clearPersonState());
+		},
+		setSnackbar: (snackbarOpen, snackbarMessage, snackbarTime) => {
+			dispatch(setSnackbar(snackbarOpen, snackbarMessage, snackbarTime));
+		},
+		updatePerson(personId, updatedPerson) {
+			return dispatch(updatePerson(personId, updatedPerson)).then((res) => {
+				if (res && res.status === 200) return res;
+			});
+		},
+		addPerson(person) {
+			return dispatch(addPerson(person)).then((res) => {
+				if (res && res.status === 201) return res;
+			});
+		}
+	};
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(withStyles(tableStyles)(PersonList));
