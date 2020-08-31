c8e2d0fe911a774294dfc997487999bee2203246
MichalStygar <misiek9753@o2.pl>
2020-08-31 14:10:00 +0200

    Create new ActivityGroupList

--- mpgk-react-frontend/src/components/ActivityGroup/ActivityGroupList.js
+++ mpgk-react-frontend/src/components/ActivityGroup/ActivityGroupList.js
@@ -1,65 +1,241 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { getGroups, deleteGroup } from "../../actions/activityGroupActions";
+import {
+	getGroups,
+    updateGroup,
+	clearGroupState
+} from "../../actions/activityGroupActions";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
-import AddButton from "../Common/AddButton";
+import {
+	Grid,
+	Typography,
+	IconButton,
+	Button,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+	Dialog,
+	LinearProgress,
+	TextField,
+	InputAdornment,
+	Tooltip
+} from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import EditIcon from "@material-ui/icons/Edit";
+import SearchIcon from "@material-ui/icons/Search";
+import { FormikTextField } from "formik-material-fields";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import { setSnackbar } from "../../reducers/snackbarReducer";
+import { tableStyles } from "./../../consts/themeConsts";
+
+const validationSchema = Yup.object().shape({
+	name: Yup.string()
+		.required("Nazwa grupy jest wymagana")
+		.max(50, "Wprowadź któtszą nazwę")
+});
 
 class ActivityGroupList extends Component {
+	state = {
+		dialogOpen: false,
+		groupId: "",
+		name: "",
+		actionType: "",
+		search: "",
+		errors: {}
+	};
+
+	updateSearch = (event) => {
+		this.setState({ search: event.target.value });
+	};
+
+	onChange = (e) => {
+		this.setState({ [e.target.name]: e.target.value });
+	};
+
+	componentWillUnmount() {
+		this.props.clearGroupState();
+	}
+
+	handleOpen = (groupId, name, actionType = "edit") => {
+		this.setState({
+			dialogOpen: true,
+            actionType: actionType,
+            name: name,
+            groupId: groupId
+		});
+	};
+
+	handleClose = () => {
+		this.setState({
+			dialogOpen: false,
+			name: "",
+			groupId: "",
+			actionType: ""
+		});
+	};
+
+	onSubmit = (values, { setSubmitting }) => {
+		setTimeout(() => {
+			setSubmitting(false);
+			if (this.state.actionType === "edit") {
+				const updatedGroup = {
+					groupId: this.state.groupId,
+					name: values.name
+				};
+
+				this.props.updateGroup(this.state.groupId, updatedGroup).then((res) => {
+					if (res) {
+						this.props.setSnackbar(true, "Dane grupy czynności zaktualizowane!");
+						this.handleClose();
+					} else {
+						this.props.setSnackbar(true, "Wystąpił błąd!");
+					}
+				});
+			} 
+		}, 500);
+	};
+
+	static getDerivedStateFromProps(nextProps, prevState) {
+		if (nextProps.errors !== prevState.errors) {
+			return { errors: nextProps.errors };
+		} else {
+			return null;
+		}
+	}
+
 	componentDidMount() {
 		this.props.getGroups();
 	}
 
-	onDeleteClick = (groupId) => {
-		this.props.deleteGroup(groupId);
-	};
-
 	render() {
+		const { classes } = this.props;
 		const { groups } = this.props.group;
+		const { errors } = this.props;
+		const filtered = groups.filter((group) => {
+			return group.name.toLowerCase().includes(this.state.search.toLowerCase());
+		});
+
 		return (
-			<div className="container mt-2">
-				<h1 className="display-4 text-center mt-2">Grupy czynności</h1>
-				<div className="row">
-					<div className="col-md-4 my-1">
-						<AddButton
-							link="/groups/add"
-							className="btn btn-info"
-							message="Dodaj grupę"
+			<>
+				<Grid container className={classes.container}>
+					<Grid item xs={12}>
+						<Typography variant="h2" className={classes.title}>
+							Grupy czynności
+						</Typography>
+					</Grid>
+					<Grid item xs={false} md={2} />
+					<Grid item xs={12} md={8} className={classes.search}>
+						<TextField
+							InputProps={{
+								startAdornment: (
+									<InputAdornment position="start">
+										<SearchIcon />
+									</InputAdornment>
+								)
+							}}
+							type="search"
+							placeholder="Szukaj..."
+							onChange={(e) => this.updateSearch(e)}
 						/>
-					</div>
-				</div>
-				<div className="table-responsive mt-2">
-					<table className="table ">
-						<thead>
-							<tr>
-								<th>Nazwa</th>
-								<th>Akcje</th>
-							</tr>
-						</thead>
-						<tbody>
-							{groups.map((group) => (
-								<tr key={group.groupId}>
-									<td>{group.name}</td>
-									<td>
-										<Link
-											to={`/groups/update/${group.groupId}`}
-											className="btn btn-primary my-1"
+					</Grid>
+					<Grid item xs={false} md={2} />
+					<Grid item xs={false} md={2} />
+					<Grid item xs={12} md={8}>
+						<TableContainer>
+							<Table>
+								<TableHead>
+									<TableRow>
+										<TableCell className={classes.head}>
+											<Typography>Nazwa</Typography>
+										</TableCell>
+										<TableCell className={classes.head}>
+											<Typography>Akcje</Typography>
+										</TableCell>
+									</TableRow>
+								</TableHead>
+								<TableBody>
+									{filtered.map((group) => (
+										<TableRow key={group.groupId}>
+											<TableCell>
+												<Typography>{group.name}</Typography>
+											</TableCell>
+											<TableCell>
+												<Tooltip title="Edytuj">
+													<IconButton
+														color="primary"
+														onClick={() =>
+															this.handleOpen(group.groupId, group.name, "edit")
+														}
+													>
+														<EditIcon />
+													</IconButton>
+												</Tooltip>
+												
+											</TableCell>
+										</TableRow>
+									))}
+								</TableBody>
+							</Table>
+						</TableContainer>
+					</Grid>
+					<Grid item xs={false} md={2} />
+				</Grid>
+
+				<Dialog open={this.state.dialogOpen} onClose={this.handleClose}>
+					<Formik
+						initialValues={{
+							name: this.state.name
+						}}
+						validationSchema={validationSchema}
+						onSubmit={(values, { setSubmitting }) =>
+							this.onSubmit(values, { setSubmitting })
+						}
+					>
+						{({ isSubmitting }) => (
+							<Form className={classes.form}>
+								<Grid container spacing={2} justify="center">
+									<Grid item xs={12}>
+										<FormikTextField
+											error={errors.name}
+											id="name"
+											name="name"
+											label="Nazwa"
+											variant="outlined"
+											required
+											helperText={errors.name}
+											onChange={this.onChange}
+										/>
+									</Grid>
+									<Grid item xs={3} />
+									<Grid item xs={3}>
+										<Button onClick={this.handleClose} color="primary">
+											Anuluj
+										</Button>
+									</Grid>
+									<Grid item xs={3}>
+										<Button
+											type="submit"
+											color="primary"
+											disabled={isSubmitting}
 										>
-											Edytuj
-										</Link>
-										<button
-											onClick={this.onDeleteClick.bind(this, group.groupId)}
-											className="btn btn-danger ml-2 my-1"
-										>
-											Usuń
-										</button>
-									</td>
-								</tr>
-							))}
-						</tbody>
-					</table>
-				</div>
-			</div>
+											Zapisz
+										</Button>
+									</Grid>
+									<Grid item xs={3} />
+									<Grid item xs={12}>
+										{isSubmitting && <LinearProgress />}
+									</Grid>
+								</Grid>
+							</Form>
+						)}
+					</Formik>
+				</Dialog>
+				
+			</>
 		);
 	}
 }
@@ -67,13 +243,39 @@ class ActivityGroupList extends Component {
 ActivityGroupList.propTypes = {
 	group: PropTypes.object.isRequired,
 	getGroups: PropTypes.func.isRequired,
-	deleteGroup: PropTypes.func.isRequired
+	updateGroup: PropTypes.func.isRequired,
+	clearGroupState: PropTypes.func.isRequired,
+	setSnackbar: PropTypes.func.isRequired
 };
 
-const mapStateToProps = (state) => ({
-	group: state.group
-});
+const mapStateToProps = (state) => {
+	return {
+		group: state.group,
+		errors: state.errors
+	};
+};
 
-export default connect(mapStateToProps, { getGroups, deleteGroup })(
-	ActivityGroupList
-);
+const mapDispatchToProps = (dispatch) => {
+	return {
+		getGroups: () => {
+			dispatch(getGroups());
+		},
+		clearGroupState: () => {
+			dispatch(clearGroupState());
+		},
+		setSnackbar: (snackbarOpen, snackbarMessage, snackbarTime) => {
+			dispatch(setSnackbar(snackbarOpen, snackbarMessage, snackbarTime));
+		},
+		updateGroup(groupId, updatedGroup) {
+			return dispatch(updateGroup(groupId, updatedGroup)).then((res) => {
+				if (res && res.status === 200) return res;
+			});
+		},
+		
+	};
+};
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(withStyles(tableStyles)(ActivityGroupList));
