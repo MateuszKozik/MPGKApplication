e3a1d365aae12c5f8e8117c78c2a5f0a9c4b030c
MichalStygar <misiek9753@o2.pl>
2020-08-31 14:21:37 +0200

    Add delete button

--- mpgk-react-frontend/src/components/ActivityGroup/ActivityGroupList.js
+++ mpgk-react-frontend/src/components/ActivityGroup/ActivityGroupList.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import {
 	getGroups,
-    updateGroup,
+	updateGroup,
+	deleteGroup,
 	clearGroupState
 } from "../../actions/activityGroupActions";
 import PropTypes from "prop-types";
@@ -26,6 +27,7 @@ import {
 import { withStyles } from "@material-ui/core/styles";
 import EditIcon from "@material-ui/icons/Edit";
 import SearchIcon from "@material-ui/icons/Search";
+import DeleteIcon from "@material-ui/icons/Delete";
 import { FormikTextField } from "formik-material-fields";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
@@ -174,7 +176,15 @@ class ActivityGroupList extends Component {
 														<EditIcon />
 													</IconButton>
 												</Tooltip>
-												
+												<Tooltip title="Usuń">
+													<IconButton
+														onClick={() =>
+															this.props.deleteGroup(group.groupId)
+														}
+													>
+														<DeleteIcon />
+													</IconButton>
+												</Tooltip>
 											</TableCell>
 										</TableRow>
 									))}
@@ -244,6 +254,7 @@ ActivityGroupList.propTypes = {
 	group: PropTypes.object.isRequired,
 	getGroups: PropTypes.func.isRequired,
 	updateGroup: PropTypes.func.isRequired,
+	deleteGroup: PropTypes.func.isRequired,
 	clearGroupState: PropTypes.func.isRequired,
 	setSnackbar: PropTypes.func.isRequired
 };
@@ -259,6 +270,9 @@ const mapDispatchToProps = (dispatch) => {
 	return {
 		getGroups: () => {
 			dispatch(getGroups());
+		},
+		deleteGroup: (groupId) => {
+			dispatch(deleteGroup(groupId));
 		},
 		clearGroupState: () => {
 			dispatch(clearGroupState());
