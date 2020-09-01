a73eccddb02724116b5c9519cc9a956152e45ae2
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-09-01 13:09:59 +0200

    Add PropTypes to the fluid registry component

--- mpgk-react-frontend/src/components/FluidRegistry/FluidRegistryList.js
+++ mpgk-react-frontend/src/components/FluidRegistry/FluidRegistryList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import {
 	getFluidRegistries,
 	deleteFluidRegistry,
@@ -357,6 +358,15 @@ class FluidRegistryList extends Component {
 		);
 	}
 }
+
+FluidRegistryList.propTypes = {
+	fluidRegistry: PropTypes.object.isRequired,
+	errors: PropTypes.object.isRequired,
+	getFluidRegistries: PropTypes.func.isRequired,
+	deleteFluidRegistry: PropTypes.func.isRequired,
+	updateFluidRegistry: PropTypes.func.isRequired,
+	setSnackbar: PropTypes.func.isRequired
+};
 
 const mapDispatchToProps = (dispatch) => {
 	return {
