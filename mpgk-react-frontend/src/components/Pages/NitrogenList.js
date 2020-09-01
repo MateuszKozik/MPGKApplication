296ee5c83d6a397d45f43dd3557861feff4a43cb
MichalStygar <misiek9753@o2.pl>
2020-09-01 14:49:08 +0200

    Chnage view nitrogen

--- mpgk-react-frontend/src/components/Pages/NitrogenList.js
+++ mpgk-react-frontend/src/components/Pages/NitrogenList.js
@@ -4,68 +4,162 @@ import {
 	getActionsByName,
 	clearInspectionsListState
 } from "../../actions/inspectionActions";
-import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core";
+import { tableStyles } from "../../consts/themeConsts";
 import FormatDate from "../Common/FormatDate";
+import {
+	Grid,
+	Typography,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+	TextField,
+	InputAdornment,
+} from "@material-ui/core";
+
+import SearchIcon from "@material-ui/icons/Search";
+
+
+
+
 
 class NitrogenList extends Component {
+
+    state = {
+        search: ""
+    }
 	componentDidMount() {
 		this.props.getActionsByName(this.props.history);
 	}
 
 	componentWillUnmount() {
 		this.props.clearInspectionsListState();
-	}
+    }
+    
+    updateSearch = (event) => {
+		this.setState({ search: event.target.value });
+	};
+
+	
 
 	render() {
-		const { inspections } = this.props.inspection;
+        
+        const { inspections } = this.props.inspection;
+        const { classes } = this.props;
+		const filtered = inspections.filter((inspection) => {
+            const {person} = inspection;
+           if(!person){
+           return inspection.datetime.includes(this.state.search.toLowerCase());
+           }
+           if(person){
+                return(
+                    inspection.datetime.includes(this.state.search.toLowerCase()) ||
+                    person.name.toLowerCase().includes(this.state.search.toLowerCase()) ||
+                    person.surname.toLowerCase().includes(this.state.search.toLowerCase())
+                );
+           } else {
+                return inspection.datetime.includes(this.state.search.toLowerCase());
+           }
+            
+		});
 
 		return (
-			<div className="container mt-2">
-				<h1 className="display-4 text-center mt-2">
-					Rejest wymiany butli z azotem w instalacji ORC
-				</h1>
-				<div className="table-responsive mt-2">
-					<table className="table mt-4">
-						<thead>
-							<tr>
-								<th>Nazwa</th>
-								<th>Data i godzina</th>
-								<th>Imię i nazwisko</th>
-							</tr>
-						</thead>
-						<tbody>
-							{inspections.map((inspection) => (
-								<tr key={inspection.inspectionId}>
-									<td>Wymiana butli</td>
-									<td>
-										<FormatDate date={inspection.datetime} datetime={true} />
-									</td>
-									<td>
-										{inspection.person
-											? inspection.person.name + " " + inspection.person.surname
-											: null}
-									</td>
-								</tr>
-							))}
-						</tbody>
-					</table>
-				</div>
-			</div>
+			<>
+				<Grid container className={classes.container}>
+					<Grid item xs={12}>
+						<Typography variant="h2" className={classes.title}>
+                            Rejest wymiany butli z azotem w instalacji ORC
+						</Typography>
+					</Grid>
+					<Grid item xs={false} md={2} />
+					<Grid item xs={12} md={8} className={classes.search}>
+						<TextField
+							InputProps={{
+								startAdornment: (
+									<InputAdornment position="start">
+										<SearchIcon />
+									</InputAdornment>
+								)
+							}}
+							type="search"
+							placeholder="Szukaj..."
+							onChange={(e) => this.updateSearch(e)}
+						/>
+					</Grid>
+					<Grid item xs={false} md={2} />
+					<Grid item xs={false} md={2} />
+					<Grid item xs={12} md={8}>
+						<TableContainer>
+							<Table>
+								<TableHead>
+									<TableRow>
+										<TableCell className={classes.head}>
+											<Typography>Nazwa</Typography>
+										</TableCell>
+										<TableCell className={classes.head}>
+											<Typography>Data i godzina</Typography>
+										</TableCell>
+										<TableCell className={classes.head}>
+											<Typography>Imię i nazwisko</Typography>
+										</TableCell>
+									</TableRow>
+								</TableHead>
+								<TableBody>
+									{filtered.map((inspection,index) => {
+                                        const{person} = inspection;
+										return (
+ 
+											<TableRow key={index}>
+												<TableCell>
+													<Typography>Wymiana butli</Typography>
+												</TableCell>
+												<TableCell>
+                                                        <FormatDate date={inspection.datetime} datetime={true} />	
+												</TableCell>
+                                                <TableCell>
+                                                    <Typography>{person && person.name + " " + person.surname}</Typography>	
+												</TableCell>
+											</TableRow>
+                                            
+										);
+									})}
+								</TableBody>
+							</Table>
+						</TableContainer>
+					</Grid>
+					<Grid item xs={false} md={2} />
+				</Grid>
+
+				
+				
+			</>
 		);
 	}
 }
 
-NitrogenList.propTypes = {
-	inspection: PropTypes.object.isRequired,
-	getActionsByName: PropTypes.func.isRequired,
-	clearInspectionsListState: PropTypes.func.isRequired
-};
+
 
 const mapStateToProps = (state) => ({
-	inspection: state.inspection
+	inspection: state.inspection,
+	errors: state.errors
 });
 
-export default connect(mapStateToProps, {
-	getActionsByName,
-	clearInspectionsListState
-})(NitrogenList);
+const mapDispatchToProps = (dispatch) => {
+	return {
+		getActionsByName: () => {
+			dispatch(getActionsByName());
+		},
+		clearInspectionsListState: () => {
+			dispatch(clearInspectionsListState());
+		},
+		
+	};
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(withStyles(tableStyles)(NitrogenList));
