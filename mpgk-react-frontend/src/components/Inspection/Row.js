a64cb5455c27518bff9a272de43a5279a0f85a63
MichalStygar <misiek9753@o2.pl>
2020-09-05 23:01:01 +0200

    Create Row

--- mpgk-react-frontend/src/components/Inspection/Row.js
+++ mpgk-react-frontend/src/components/Inspection/Row.js
@@ -3,30 +3,21 @@ import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import Box from '@material-ui/core/Box';
 import Collapse from '@material-ui/core/Collapse';
+import FormatDate from "../Common/FormatDate";
 import {
-    Grid,
-    Typography,
     IconButton,
-    Fab,
-    Button,
     Table,
     TableBody,
     TableCell,
-    TableContainer,
     TableHead,
-    TableRow,
-    Dialog,
-    LinearProgress,
-    TextField,
-    InputAdornment,
-    Tooltip
+    TableRow
 } from "@material-ui/core";
 
 
 function Row(inspection) {
     const { row } = inspection;
-    const {inspections} = inspection;
     const [open, setOpen] = React.useState(false);
+    
     
   
     return (
@@ -40,7 +31,7 @@ function Row(inspection) {
           <TableCell component="th" scope="row">
             {row.status}
           </TableCell>
-          <TableCell align="left">{row.activityGroup.name}</TableCell>
+          <TableCell align="center" style={{ fontSize: 18 }} >{row.activityGroup.name}</TableCell>
           
         </TableRow>
         <TableRow>
@@ -51,27 +42,28 @@ function Row(inspection) {
                 <Table size="small" aria-label="purchases">
                   <TableHead>
                     <TableRow>
-                      <TableCell>Status</TableCell>
+                      <TableCell>Nazwa aktywności</TableCell>
                       <TableCell>Data wykonania</TableCell>
-                      <TableCell align="right">Parametr</TableCell>
-                      <TableCell align="right">Uwagi</TableCell>
-                      <TableCell align="right">Pracownik</TableCell>
+                      <TableCell align="left">Parametr</TableCell>
+                      <TableCell align="left">Uwagi</TableCell>
+                      <TableCell align="left">Status</TableCell>
+                      <TableCell align="left">Pracownik</TableCell>
                     </TableRow>
                   </TableHead>
                   <TableBody>
-                    
-                      <TableRow >
+                  {row.inspections.map((inspection, i) => (
+                      <TableRow key={i}>
                         <TableCell component="th" scope="row">
-                          {row.status}
-                          {console.log(row.connection)}
+                          {inspection.activity && inspection.activity.name}
                         </TableCell>
-                        <TableCell>{row.inspectionId}</TableCell>
-                        <TableCell align="right">{row.startTime}</TableCell>
-                        <TableCell align="right">
-                          {row.endTime}
-                        </TableCell>
+                        <TableCell><FormatDate date={inspection.datetime} /></TableCell>
+                        <TableCell align="left">{inspection.parameter === "true" ? "Wykonany" :inspection.parameter !== null ? inspection.parameter: "Nie wykonany"}</TableCell>
+                        <TableCell align="left">{inspection.comment}</TableCell>
+                        <TableCell align="left">{inspection.status}</TableCell>
+                  <TableCell align="left">{inspection.person && inspection.person.name+ " " + inspection.person.surname} {inspection.supervisor && inspection.supervisor.name+" "+inspection.supervisor.surname}</TableCell>
                       </TableRow>
-                    
+                     
+                    ))}
                   </TableBody>
                 </Table>
               </Box>
