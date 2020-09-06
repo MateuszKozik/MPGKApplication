00be5f019a4c8a77474fa67c05ea51d0361e04db
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-09-06 13:28:20 +0200

    Remove unused imports

--- mpgk-backend/src/main/java/com/kozik/MPGK/entities/Connection.java
+++ mpgk-backend/src/main/java/com/kozik/MPGK/entities/Connection.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -18,7 +17,6 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Data;
