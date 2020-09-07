86b4c06a655db9bf7c91dd70dcf5e4c62bdcc49a
MichalStygar <misiek9753@o2.pl>
2020-09-07 17:53:17 +0200

    Create PerformersList

--- mpgk-react-frontend/src/components/Pages/PerformersList.js
+++ mpgk-react-frontend/src/components/Pages/PerformersList.js
@@ -1,53 +1,153 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { getPersons } from "../../actions/personActions";
+import {
+	getConnections,
+	clearConnectionState
+} from "../../actions/connectionActions";
+import { withStyles } from "@material-ui/core";
+import { tableStyles } from "../../consts/themeConsts";
 import PropTypes from "prop-types";
+import {
+	Grid,
+	Typography,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+	TextField,
+	InputAdornment,
+} from "@material-ui/core";
+
+import SearchIcon from "@material-ui/icons/Search";
+
+
 
 
 
 class PerformersList extends Component {
+
+    state = {
+        search: ""
+    }
 	componentDidMount() {
-		this.props.getPersons();
+		this.props.getConnections();
 	}
 
+	componentWillUnmount() {
+		this.props.clearConnectionState();
+    }
+    
+    updateSearch = (event) => {
+		this.setState({ search: event.target.value });
+	};
+
+	
 
 	render() {
-		const { persons } = this.props.person;
+        
+        const { connections } = this.props.connection;
+        const { classes } = this.props;
+		const filtered = connections.filter((connection) => {
+           return connection.name.toLowerCase().includes(this.state.search.toLowerCase());   
+        });
+
 		return (
-			<div className="container mt-2">
-				<h1 className="display-4 text-center mt-2">Lista wykonawców</h1>			
-				<div className="table-responsive mt-2">
-					<table className="table mt-4">
-						<thead>
-							<tr>
-								<th>Imię</th>
-								<th>Nazwisko</th>
-							</tr>
-						</thead>
-						<tbody>
-							{persons.map((person) => (
-								<tr key={person.personId}>
-									<td>{person.name}</td>
-									<td>{person.surname}</td>
-								</tr>
-							))}
-						</tbody>
-					</table>
-				</div>
-			</div>
+			<>
+				<Grid container className={classes.container}>
+					<Grid item xs={12}>
+						<Typography variant="h2" className={classes.title}>
+                            Lista wykonawców
+						</Typography>
+					</Grid>
+					<Grid item xs={false} md={2} />
+					<Grid item xs={12} md={8} className={classes.search}>
+						<TextField
+							InputProps={{
+								startAdornment: (
+									<InputAdornment position="start">
+										<SearchIcon />
+									</InputAdornment>
+								)
+							}}
+							type="search"
+							placeholder="Szukaj..."
+							onChange={(e) => this.updateSearch(e)}
+						/>
+					</Grid>
+					<Grid item xs={false} md={2} />
+					<Grid item xs={false} md={2} />
+					<Grid item xs={12} md={8}>
+						<TableContainer>
+							<Table>
+								<TableHead>
+									<TableRow>
+										<TableCell className={classes.head}>
+											<Typography>Rodzaj przeglądu</Typography>
+										</TableCell>
+										<TableCell className={classes.head}>
+											<Typography>Imię i nazwisko</Typography>
+										</TableCell>
+									</TableRow>
+								</TableHead>
+								<TableBody>
+									{filtered.map((connection,index) => {
+                                        const {persons} = connection;
+										return (
+ 
+											<TableRow key={index}>
+												<TableCell>
+													<Typography>{connection.name}</Typography>
+												</TableCell>
+												<TableCell>
+                                                    {persons.map((person,i) => { return(
+                                                        <Typography key={i}>
+                                                            {person && person.name + " " + person.surname} 
+                                                        </Typography>
+                                                        )})}	
+												</TableCell>                                     
+											</TableRow>
+                                            
+										);
+									})}
+								</TableBody>
+							</Table>
+						</TableContainer>
+					</Grid>
+					<Grid item xs={false} md={2} />
+				</Grid>
+
+				
+				
+			</>
 		);
 	}
 }
 
 PerformersList.propTypes = {
-	person: PropTypes.object.isRequired,
-	getPersons: PropTypes.func.isRequired
+	connection: PropTypes.object.isRequired,
+	getConnections: PropTypes.func.isRequired,
+	clearConnectionState: PropTypes.func.isRequired
 };
 
 const mapStateToProps = (state) => ({
-	person: state.person
+	connection: state.connection
 });
 
-export default connect(mapStateToProps, { getPersons })(
-	PerformersList
-);
+const mapDispatchToProps = (dispatch) => {
+	return {
+		getConnections: () => {
+			dispatch(getConnections());
+		},
+		clearConnectionState: () => {
+			dispatch(clearConnectionState());
+		},
+		
+	};
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(withStyles(tableStyles)(PerformersList));
