fd3f252bccce93e5b09d52ca429deccd31625a6d
MichalStygar <misiek9753@o2.pl>
2020-09-08 14:36:42 +0200

    Change import to actions and reducers and theme

--- mpgk-react-frontend/src/components/Pages/Admin/FluidPlaceList.js
+++ mpgk-react-frontend/src/components/Pages/Admin/FluidPlaceList.js
@@ -6,9 +6,9 @@ import {
 	addFluidPlace,
 	updateFluidPlace,
 	clearFluidPlaceState
-} from "../../actions/fluidPlaceActions";
+} from "../../../actions/fluidPlaceActions";
 import { withStyles } from "@material-ui/core";
-import { tableStyles } from "../../consts/themeConsts";
+import { tableStyles } from "../../../consts/themeConsts";
 import {
 	Grid,
 	Typography,
@@ -33,7 +33,7 @@ import SearchIcon from "@material-ui/icons/Search";
 import { FormikTextField } from "formik-material-fields";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
-import { setSnackbar } from "../../reducers/snackbarReducer";
+import { setSnackbar } from "../../../reducers/snackbarReducer";
 
 const validationSchema = Yup.object().shape({
 	name: Yup.string()
