aecba5c6dfe97df5797cbcbf19559cb155dfd700
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-09-08 10:09:44 +0200

    Enable Spring Security

--- mpgk-backend/src/main/java/com/kozik/MPGK/MpgkApplication.java
+++ mpgk-backend/src/main/java/com/kozik/MPGK/MpgkApplication.java
@@ -2,11 +2,10 @@ package com.kozik.MPGK;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class MpgkApplication {
 
 	public static void main(String[] args) {
