af8ac5a67683928f6f1a3bfaa31b65437addc32e
MichalStygar <misiek9753@o2.pl>
2020-09-08 16:21:23 +0200

    Remove get_group variable

--- mpgk-react-frontend/src/actions/types.js
+++ mpgk-react-frontend/src/actions/types.js
@@ -24,7 +24,6 @@ export const UPDATE_PERSON = "UPDATE_PERSON";
 //activity group
 export const ADD_GROUP = "ADD_GROUP";
 export const GET_GROUPS = "GET_GROUPS";
-export const GET_GROUP = "GET_GROUP";
 export const DELETE_GROUP = "DELETE_GROUP";
 export const UPDATE_GROUP = "UPDATE_GROUP";
 
