d609a8eaea80433b71d17854c4fb8c311ac7d7a7
MichalStygar <misiek9753@o2.pl>
2020-09-08 17:17:51 +0200

    Remove get_person variable

--- mpgk-react-frontend/src/actions/types.js
+++ mpgk-react-frontend/src/actions/types.js
@@ -15,7 +15,6 @@ export const DELETE_FLUID = "DELETE_FLUID";
 //persons
 export const ADD_PERSON = "ADD_PERSON";
 export const GET_PERSONS = "GET_PERSONS";
-export const GET_PERSON = "GET_PERSON";
 export const DELETE_PERSON = "DELETE_PERSON";
 export const UPDATE_PERSON = "UPDATE_PERSON";
 
