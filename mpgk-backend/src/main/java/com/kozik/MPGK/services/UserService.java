36dab6757f8b9109d428f886d952af0b5d9bf7d5
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-09-09 11:38:15 +0200

    Create user service

--- mpgk-backend/src/main/java/com/kozik/MPGK/services/UserService.java
+++ mpgk-backend/src/main/java/com/kozik/MPGK/services/UserService.java
@@ -1,50 +1,29 @@
 package com.kozik.MPGK.services;
 
 import com.kozik.MPGK.repositories.UserRepository;
-import com.kozik.MPGK.entities.Role;
 import com.kozik.MPGK.entities.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import com.kozik.MPGK.exceptions.userExceptions.UsernameAlreadyExistsException;
 
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    @Autowired private UserRepository userRepository;
 
-    public List<User> listAll(){
-        return userRepository.findAll();
-    }
+    @Autowired
+    private UserRepository userRepository;
 
-    public void save(User user,List<Role> role){
-        user.setRole(role);
-        userRepository.save(user);
-    }
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public void savee(User user){
-        userRepository.save(user);
-    }
-
-    public User get(String username){
-        return userRepository.findById(username).get();
-    }
-
-    public void delete(String username){
-        userRepository.deleteById(username);
-    }
-
-    public Boolean isUserExist(String username){
-        return userRepository.existsById(username);
-    }
-
-    public User update(String username,User user){
-        User currentUser = get(username);
-        currentUser.setUsername(user.getUsername());
-        currentUser.setPassword(user.getPassword());
-        currentUser.setEnabled(user.getEnabled());
-        savee(currentUser);
-
-        return currentUser;
-
+    public User saveUser(User newUser) {
+        try {
+            newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+            newUser.setConfirmPassword("");
+            return userRepository.save(newUser);
+        } catch (Exception e) {
+            throw new UsernameAlreadyExistsException(newUser.getUsername());
+        }
     }
 }
