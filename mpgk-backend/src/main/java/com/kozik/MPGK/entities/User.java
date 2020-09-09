cc3869d5fc775e2ae178f55c432eab58e175a8ea
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-09-09 10:56:14 +0200

    User entity implements UserDetails

--- mpgk-backend/src/main/java/com/kozik/MPGK/entities/User.java
+++ mpgk-backend/src/main/java/com/kozik/MPGK/entities/User.java
@@ -1,15 +1,30 @@
 package com.kozik.MPGK.entities;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,25 +33,71 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @Table(name = "users")
-public class User {
-    
+public class User implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
+
     @Id
-    @Column(name = "username", nullable = false, length = 35)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @NotBlank(message = "Nazwa użytkownika jest wymagana")
+    @Column(name = "username", nullable = false, length = 35, unique = true)
     private String username;
 
+    @NotBlank(message = "Hasło jest wymagane")
     @Column(name = "password", nullable = false, length = 60)
     private String password;
+
+    @Transient
+    private String confirmPassword;
 
     @Column(name = "enabled", nullable = false)
     private Boolean enabled = true;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "users_roles", joinColumns = {
-        @JoinColumn(name = "user_username", referencedColumnName = "username")},
-        inverseJoinColumns = {
-            @JoinColumn(name ="role_name", referencedColumnName = "name")})
-    private List<Role> role;
+            @JoinColumn(name = "user_id", referencedColumnName = "user_id") }, inverseJoinColumns = {
+                    @JoinColumn(name = "role_id", referencedColumnName = "role_id") })
+    private List<Role> role = new ArrayList<>();
 
-    @OneToOne(mappedBy = "user")
+    @OneToOne(mappedBy = "user", fetch = FetchType.EAGER)
+    @JsonProperty(access = Access.WRITE_ONLY)
     private Person person;
+
+    // UserDetails interface methods
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        getRole().forEach(item -> {
+            authorities.add(new SimpleGrantedAuthority("ROLE_" + item.getName()));
+        });
+        return authorities;
+    }
+
 }
