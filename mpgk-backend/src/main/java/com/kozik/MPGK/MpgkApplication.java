d360d26858c55e39d8db7d440c171e7514a9ac9e
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-09-09 10:54:20 +0200

    Add BCryptPasswordEncoder bean

--- mpgk-backend/src/main/java/com/kozik/MPGK/MpgkApplication.java
+++ mpgk-backend/src/main/java/com/kozik/MPGK/MpgkApplication.java
@@ -2,11 +2,18 @@ package com.kozik.MPGK;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableScheduling
 @SpringBootApplication
 public class MpgkApplication {
+
+	@Bean
+	BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MpgkApplication.class, args);
