75b15e0edd58b8fc0dea28afaa0801d40ef9e35d
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-09-09 11:13:10 +0200

    Create class to store security constraints

---
+++ mpgk-backend/src/main/java/com/kozik/MPGK/security/SecurityConstraints.java
@@ -0,0 +1,10 @@
+package com.kozik.MPGK.security;
+
+public class SecurityConstraints {
+
+    public static final String SIGN_UP_URLS = "/api/users/**";
+    public static final String SECRET = "MPGKKrosno";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final long EXPIRATION_TIME = 600_000; // 10 minutes expiration time
+}
