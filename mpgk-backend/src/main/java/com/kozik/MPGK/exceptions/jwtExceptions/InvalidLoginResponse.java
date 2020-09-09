8991b1a20f7f3881344634b37b854fa1f0f267df
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-09-09 21:15:39 +0200

    Change error messages

--- mpgk-backend/src/main/java/com/kozik/MPGK/exceptions/jwtExceptions/InvalidLoginResponse.java
+++ mpgk-backend/src/main/java/com/kozik/MPGK/exceptions/jwtExceptions/InvalidLoginResponse.java
@@ -8,7 +8,7 @@ public class InvalidLoginResponse {
     private String password;
 
     public InvalidLoginResponse() {
-        this.username = "Invalid Username";
-        this.password = "Invalid Password";
+        this.username = "Błędny login";
+        this.password = "Błędne hasło";
     }
 }
