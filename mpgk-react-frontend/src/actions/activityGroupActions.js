24d46e7a960a9cb0aaeb69b706ee09897a430e17
MichalStygar <misiek9753@o2.pl>
2020-09-10 16:36:21 +0200

    Add hostName

--- mpgk-react-frontend/src/actions/activityGroupActions.js
+++ mpgk-react-frontend/src/actions/activityGroupActions.js
@@ -7,11 +7,12 @@ import {
 	UPDATE_GROUP
 } from "./types";
 import isUserLogin from "../securityUtils/isUserLogin";
+import {hostName} from "./host";
 
 export const addGroup = (group) => async (dispatch) => {
 	try {
 		if (isUserLogin()) {
-			const res = await axios.post("/api/groups", group);
+			const res = await axios.post(`${hostName}/api/groups`, group);
 			dispatch({
 				type: ADD_GROUP,
 				payload: res.data
@@ -32,7 +33,7 @@ export const addGroup = (group) => async (dispatch) => {
 
 export const getGroups = () => async (dispatch) => {
 	if (isUserLogin()) {
-		const res = await axios.get("/api/groups");
+		const res = await axios.get(`${hostName}/api/groups`);
 		dispatch({
 			type: GET_GROUPS,
 			payload: res.data
@@ -43,7 +44,7 @@ export const getGroups = () => async (dispatch) => {
 export const updateGroup = (groupId, updatedGroup) => async (dispatch) => {
 	try {
 		if (isUserLogin()) {
-			const res = await axios.put(`/api/groups/${groupId}`, updatedGroup);
+			const res = await axios.put(`${hostName}/api/groups/${groupId}`, updatedGroup);
 			dispatch({
 				type: UPDATE_GROUP,
 				payload: res.data
@@ -65,7 +66,7 @@ export const updateGroup = (groupId, updatedGroup) => async (dispatch) => {
 export const deleteGroup = (groupId) => async (dispatch) => {
 	if (isUserLogin()) {
 		if (window.confirm("Czy jesteś pewny? Spowoduje to usunięcie grupy")) {
-			await axios.delete(`/api/groups/${groupId}`);
+			await axios.delete(`${hostName}/api/groups/${groupId}`);
 			dispatch({
 				type: DELETE_GROUP,
 				payload: groupId
