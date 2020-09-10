63461d1ce73506aefbe718bcfc9e96d799735d7a
MichalStygar <misiek9753@o2.pl>
2020-09-10 16:20:59 +0200

    Add hostName

--- mpgk-react-frontend/src/actions/inspectionTypeActions.js
+++ mpgk-react-frontend/src/actions/inspectionTypeActions.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { GET_INSPECTION_TYPES } from "./types";
 import isUserLogin from "../securityUtils/isUserLogin";
+import {hostName} from "./host";
 
 export const getInspectionTypes = () => async (dispatch) => {
 	if (isUserLogin()) {
-		const res = await axios.get("/api/inspection-types");
+		const res = await axios.get(`${hostName}/api/inspection-types`);
 		dispatch({
 			type: GET_INSPECTION_TYPES,
 			payload: res.data
