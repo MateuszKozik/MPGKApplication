63b065c85bd272fd4890fceb1d41e462036c297a
MichalStygar <misiek9753@o2.pl>
2020-09-11 15:04:22 +0200

    Create taskActions

---
+++ mpgk-react-frontend/src/actions/taskActions.js
@@ -0,0 +1,88 @@
+import axios from "axios";
+
+import isUserLogin from "../securityUtils/isUserLogin";
+import {hostName} from "./host";
+
+export const createDailyInspections = (connectionId, history) => async (
+	dispatch
+) => {
+	if (isUserLogin()) {
+        if (window.confirm("Czy na pewno chcesz wygenerować przegląd?")) {
+			try {
+                const res = await axios.post(`${hostName}/api/tasks/daily/${connectionId}`); 
+                history.push("/connections");
+                return res;
+			} catch (error) {
+				history.push("/");
+            }	
+        }
+	}
+};
+
+export const createWeeklyInspections = (connectionId, history) => async (
+	dispatch
+) => {
+	if (isUserLogin()) {
+        if (window.confirm("Czy na pewno chcesz wygenerować przegląd?")) {
+			try {
+                const res = await axios.post(`${hostName}/api/tasks/weekly/${connectionId}`); 
+                history.push("/connections");
+                return res;
+			} catch (error) {
+				history.push("/");
+            }	
+        }
+	}
+};
+
+export const createDayShiftInspections = (connectionId, history) => async (
+	dispatch
+) => {
+	if (isUserLogin()) {
+        if (window.confirm("Czy na pewno chcesz wygenerować przegląd?")) {
+			try {
+                const res = await axios.post(`${hostName}/api/tasks/day-shift/${connectionId}`); 
+                history.push("/connections");
+                return res;
+			} catch (error) {
+				history.push("/");
+            }	
+        }
+	}
+};
+
+export const createTwoMonthsInspections = (connectionId, history) => async (
+	dispatch
+) => {
+	if (isUserLogin()) {
+        if (window.confirm("Czy na pewno chcesz wygenerować przegląd?")) {
+			try {
+                const res = await axios.post(`${hostName}/api/tasks/two-months/${connectionId}`); 
+                history.push("/connections");
+                return res;
+			} catch (error) {
+				history.push("/");
+            }
+        }	
+	}
+};
+
+export const createYearlyInspections = (connectionId, history) => async (
+	dispatch
+) => {
+	if (isUserLogin()) {
+        if (window.confirm("Czy na pewno chcesz wygenerować przegląd?")) {
+			try {
+                const res = await axios.post(`${hostName}/api/tasks/yearly/${connectionId}`); 
+                history.push("/connections");
+                return res;
+			} catch (error) {
+				history.push("/");
+            }
+        }	
+	}
+};
+
+
+
+
