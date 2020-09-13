8e13ddf3e222de0965a51d9e3aabc6fd89e126cf
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-09-13 19:36:50 +0200

    Set jwt token expiration time to the 30 minutes

--- mpgk-backend/src/main/java/com/kozik/MPGK/security/SecurityConstraints.java
+++ mpgk-backend/src/main/java/com/kozik/MPGK/security/SecurityConstraints.java
@@ -6,5 +6,5 @@ public class SecurityConstraints {
     public static final String SECRET = "MPGKKrosno";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final long EXPIRATION_TIME = 600_000; // 10 minutes expiration time
+    public static final long EXPIRATION_TIME = 1800000; // 30 minutes expiration time
 }
