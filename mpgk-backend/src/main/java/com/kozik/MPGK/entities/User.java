f774bf1505026009806434cca4a00d15a636b634
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-09-13 10:41:43 +0200

    Set cascade type to all in the user entity

--- mpgk-backend/src/main/java/com/kozik/MPGK/entities/User.java
+++ mpgk-backend/src/main/java/com/kozik/MPGK/entities/User.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -62,7 +63,7 @@ public class User implements UserDetails {
                     @JoinColumn(name = "role_id", referencedColumnName = "role_id") })
     private List<Role> role = new ArrayList<>();
 
-    @OneToOne(mappedBy = "user", fetch = FetchType.EAGER)
+    @OneToOne(mappedBy = "user", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JsonProperty(access = Access.WRITE_ONLY)
     private Person person;
 
