47687fa6e8da0425b71af42bcbcad8c13a33b1a3
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-09-16 20:07:19 +0200

    Add print button to the inspection activity component

--- mpgk-react-frontend/src/components/Pages/InspectionActivities.js
+++ mpgk-react-frontend/src/components/Pages/InspectionActivities.js
@@ -3,9 +3,10 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { getActivitiesByConnection } from "../../actions/activityActions";
 import { clearInspectionsListState } from "../../actions/inspectionActions";
-import { Grid, Typography, Divider } from "@material-ui/core";
+import { Grid, Typography, Divider, Button } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 import { tableStyles } from "./../../consts/themeConsts";
+import { generateActivityReport } from "../../actions/pdfGeneratorActions";
 
 class InspectionActivities extends Component {
 	componentDidMount() {
@@ -22,63 +23,93 @@ class InspectionActivities extends Component {
 		const { classes } = this.props;
 		const { validToken, user } = this.props.security;
 		const { authorities } = user;
+		const { connectionId } = this.props.match.params;
+
 		return (
 			<>
-				{inspectionsList && inspectionsList.map((activitiesList, i) => (
-					<Grid key={i} container className={classes.form}>
+				{inspectionsList && (
+					<Grid
+						container
+						className={classes.form}
+						style={{ textAlign: "left" }}
+					>
 						<Grid item xs={12}>
-							<Typography variant="h5" align="center">
-								{activitiesList.activityGroup.name}
-							</Typography>
+							<Button
+								variant="contained"
+								color="primary"
+								onClick={() => {
+									this.props.generateActivityReport(connectionId, "Czynności");
+								}}
+							>
+								Drukuj
+							</Button>
 						</Grid>
+					</Grid>
+				)}
 
-						{activitiesList && activitiesList.activities.map((activity, j) => (
-							
-							<Grid item xs={12} key={j}>
-								<Grid container spacing={2} className={classes.container}>
-								{validToken && user && authorities !== "ROLE_ADMIN" ? (
-									<Grid item xs={12} md={8}>
-										<Typography align="justify">{activity.name}</Typography>
-									</Grid>
-									):
-									<Grid item xs={12} md={4}>
-										<Typography align="justify">{activity.name}</Typography>
-									</Grid>
-									}
-									{validToken && user && authorities === "ROLE_ADMIN" ? (
-										
-										<Grid item xs={12} md={2}>
-											{activity.type && (
-												<Typography>{activity.type}</Typography>
+				{inspectionsList &&
+					inspectionsList.map((activitiesList, i) => (
+						<Grid key={i} container className={classes.form}>
+							<Grid item xs={12}>
+								<Typography variant="h5" align="center">
+									{activitiesList.activityGroup.name}
+								</Typography>
+							</Grid>
+
+							{activitiesList &&
+								activitiesList.activities.map((activity, j) => (
+									<Grid item xs={12} key={j}>
+										<Grid container spacing={2} className={classes.container}>
+											{validToken && user && authorities !== "ROLE_ADMIN" ? (
+												<Grid item xs={12} md={8}>
+													<Typography align="justify">
+														{activity.name}
+													</Typography>
+												</Grid>
+											) : (
+												<Grid item xs={12} md={4}>
+													<Typography align="justify">
+														{activity.name}
+													</Typography>
+												</Grid>
 											)}
-										</Grid>
-									): null}
-									<Grid item xs={12} md={2}>
-										{activity.emsr && <Typography>{activity.emsr}</Typography>}
-									</Grid>
-									<Grid item xs={12} md={2}>
-										{activity.setting && (
-											<Typography>{activity.setting}</Typography>
-										)}
-									</Grid>
-									{validToken && user && authorities === "ROLE_ADMIN" ? (
-										<Grid item xs={12} md={2}>
-												{activity.listItems && (
-													<>
-													<Typography><b>Elementy listy:</b></Typography>
-													<Typography>{activity.listItems}</Typography>
-													</>
+											{validToken && user && authorities === "ROLE_ADMIN" ? (
+												<Grid item xs={12} md={2}>
+													{activity.type && (
+														<Typography>{activity.type}</Typography>
+													)}
+												</Grid>
+											) : null}
+											<Grid item xs={12} md={2}>
+												{activity.emsr && (
+													<Typography>{activity.emsr}</Typography>
 												)}
 											</Grid>
-									): null}
-									<Grid item xs={12}>
-										<Divider />
+											<Grid item xs={12} md={2}>
+												{activity.setting && (
+													<Typography>{activity.setting}</Typography>
+												)}
+											</Grid>
+											{validToken && user && authorities === "ROLE_ADMIN" ? (
+												<Grid item xs={12} md={2}>
+													{activity.listItems && (
+														<>
+															<Typography>
+																<b>Elementy listy:</b>
+															</Typography>
+															<Typography>{activity.listItems}</Typography>
+														</>
+													)}
+												</Grid>
+											) : null}
+											<Grid item xs={12}>
+												<Divider />
+											</Grid>
+										</Grid>
 									</Grid>
-								</Grid>
-							</Grid>
-						))}
-					</Grid>
-				))}
+								))}
+						</Grid>
+					))}
 			</>
 		);
 	}
@@ -87,7 +118,8 @@ class InspectionActivities extends Component {
 InspectionActivities.propTypes = {
 	inspection: PropTypes.object.isRequired,
 	getActivitiesByConnection: PropTypes.func.isRequired,
-	clearInspectionsListState: PropTypes.func.isRequired
+	clearInspectionsListState: PropTypes.func.isRequired,
+	generateActivityReport: PropTypes.func.isRequired
 };
 
 const mapDispatchToProps = (dispatch) => {
@@ -97,6 +129,9 @@ const mapDispatchToProps = (dispatch) => {
 		},
 		clearInspectionsListState: () => {
 			dispatch(clearInspectionsListState());
+		},
+		generateActivityReport: (connectionId, fileName) => {
+			dispatch(generateActivityReport(connectionId, fileName));
 		}
 	};
 };
