61b64f0d51d672fce0b993925c3ace450d4fbf34
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-09-22 21:27:42 +0200

    Add swagger annotation to the activity controller

--- mpgk-backend/src/main/java/com/kozik/MPGK/controllers/ActivityController.java
+++ mpgk-backend/src/main/java/com/kozik/MPGK/controllers/ActivityController.java
@@ -14,6 +14,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 import com.kozik.MPGK.services.MapValidationErrorService;
 
 import javax.validation.Valid;
@@ -22,6 +27,7 @@ import org.springframework.validation.BindingResult;
 
 @RestController
 @CrossOrigin
+@Api(tags = "activities", description = "Operations about activity")
 @RequestMapping("/api/activities")
 public class ActivityController {
 
@@ -32,20 +38,25 @@ public class ActivityController {
     private MapValidationErrorService mapValidationErrorService;
 
     // Get all activities
+    @ApiOperation(value = "Get all activities")
     @GetMapping("")
     public Iterable<Activity> getActivities() {
         return activityService.listAll();
     }
 
     // Get single activity
+    @ApiOperation(value = "Get activity by id")
     @GetMapping("/{activityId}")
-    public ResponseEntity<?> getActivity(@PathVariable Long activityId) {
+    public ResponseEntity<Activity> getActivity(
+            @ApiParam(value = "Unique id of activity", example = "123") @PathVariable Long activityId) {
         return new ResponseEntity<>(activityService.get(activityId), HttpStatus.OK);
     }
 
     // Create activity
+    @ApiOperation(value = "Create new activity")
     @PostMapping("")
-    public ResponseEntity<?> createActivity(@Valid @RequestBody Activity activity, BindingResult result) {
+    public ResponseEntity<?> createActivity(
+            @ApiParam(value = "Created activity object") @Valid @RequestBody Activity activity, BindingResult result) {
         if (result.hasErrors()) {
             return mapValidationErrorService.MapValidationService(result);
         }
@@ -54,9 +65,11 @@ public class ActivityController {
     }
 
     // Update activity
+    @ApiOperation(value = "Update activity")
     @PutMapping("/{activityId}")
-    public ResponseEntity<?> updateupdateActivityFluid(@PathVariable Long activityId,
-            @Valid @RequestBody Activity activity, BindingResult result) {
+    public ResponseEntity<?> updateupdateActivityFluid(
+            @ApiParam(value = "Id that need to be updated", example = "123") @PathVariable Long activityId,
+            @ApiParam(value = "Update activity object") @Valid @RequestBody Activity activity, BindingResult result) {
         if (result.hasErrors()) {
             return mapValidationErrorService.MapValidationService(result);
         }
@@ -65,16 +78,20 @@ public class ActivityController {
     }
 
     // Delete activity
+    @ApiOperation(value = "Delete activity")
     @DeleteMapping("/{activityId}")
-    public ResponseEntity<?> deleteActivity(@PathVariable Long activityId) {
+    public ResponseEntity<?> deleteActivity(
+            @ApiParam(value = "Id that need to be deleted", example = "123") @PathVariable Long activityId) {
         activityService.delete(activityId);
         return new ResponseEntity<>(new Message("Activity with id: " + activityId + " has been removed."),
                 HttpStatus.OK);
     }
 
     // Get activities by connection
+    @ApiOperation(value = "Get activities by connection id")
     @GetMapping("/list/{connectionId}")
-    public ResponseEntity<?> getInspectionsByConnection(@PathVariable Long connectionId) {
+    public ResponseEntity<?> getInspectionsByConnection(
+            @ApiParam(value = "Unique connection id", example = "123") @PathVariable Long connectionId) {
         return new ResponseEntity<>(activityService.getActivitiesByConnection(connectionId), HttpStatus.OK);
     }
 }
