19fa73f23ce9aab72e862e2a1969eeb3314e79a8
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-09-23 08:33:06 +0200

    Add swagger api annotations to the homepage controller

--- mpgk-backend/src/main/java/com/kozik/MPGK/controllers/HomePageController.java
+++ mpgk-backend/src/main/java/com/kozik/MPGK/controllers/HomePageController.java
@@ -11,14 +11,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @CrossOrigin
+@Api(tags = "homepage", description = "Operations about homepage")
 @RequestMapping("/api/home")
 public class HomePageController {
 
     @Autowired
     private ConnectionService connectionService;
 
+    @ApiOperation(value = "Get all inspection for the homepage")
     @GetMapping("/connections")
     public List<ConnectionObject> getConnections() {
         return connectionService.getHomePageConnections();
