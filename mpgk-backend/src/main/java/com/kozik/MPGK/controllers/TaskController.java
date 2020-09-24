c98c5e39b91afeacc8f075121c32b9d714e3e00d
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-09-24 09:00:06 +0200

    Add swagger api annotations to the task controller

--- mpgk-backend/src/main/java/com/kozik/MPGK/controllers/TaskController.java
+++ mpgk-backend/src/main/java/com/kozik/MPGK/controllers/TaskController.java
@@ -14,8 +14,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import springfox.documentation.annotations.ApiIgnore;
+
 @RestController
 @CrossOrigin
+@Api(tags = "tasks", description = "Operations about generating tasks")
 @RequestMapping("/api/tasks")
 public class TaskController {
 
@@ -23,43 +29,56 @@ public class TaskController {
     private TaskService taskService;
 
     // Generate daily inspections
+    @ApiOperation(value = "Generate daily inspections by connection id")
     @PostMapping("/daily/{connectionId}")
-    public ResponseEntity<?> crateDailyInspections(@PathVariable Long connectionId) {
+    public ResponseEntity<?> crateDailyInspections(
+            @ApiParam(value = "Unique id of connection", example = "123") @PathVariable Long connectionId) {
         taskService.daily(connectionId);
         return new ResponseEntity<>(new Message("Daily inspections have been generated."), HttpStatus.CREATED);
     }
 
     // Generate weekly inspections
+    @ApiOperation(value = "Generate weekly inspections by connection id")
     @PostMapping("/weekly/{connectionId}")
-    public ResponseEntity<?> createWeeklyInspections(@PathVariable Long connectionId) {
+    public ResponseEntity<?> createWeeklyInspections(
+            @ApiParam(value = "Unique id of connection", example = "123") @PathVariable Long connectionId) {
         taskService.weekly(connectionId);
         return new ResponseEntity<>(new Message("Weekly inspections have been generated."), HttpStatus.CREATED);
     }
 
     // Generate day shift inspections
+    @ApiOperation(value = "Generate day shift inspections by connection id")
     @PostMapping("/day-shift/{connectionId}")
-    public ResponseEntity<?> createDayShiftInspections(@PathVariable Long connectionId) {
+    public ResponseEntity<?> createDayShiftInspections(
+            @ApiParam(value = "Unique id of connection", example = "123") @PathVariable Long connectionId) {
         taskService.dayShift(connectionId);
         return new ResponseEntity<>(new Message("Day shift inspections have been generated."), HttpStatus.CREATED);
     }
 
-    // Generate two monts inspections
+    // Generate two months inspections
+    @ApiOperation(value = "Generate two months inspections by connection id")
     @PostMapping("/two-months/{connectionId}")
-    public ResponseEntity<?> createTwoMonthsInspections(@PathVariable Long connectionId) {
+    public ResponseEntity<?> createTwoMonthsInspections(
+            @ApiParam(value = "Unique id of connection", example = "123") @PathVariable Long connectionId) {
         taskService.everyTwoMonths(connectionId);
         return new ResponseEntity<>(new Message("Two months inspections have been generated."), HttpStatus.CREATED);
     }
 
     // Generate yearly inspections
+    @ApiOperation(value = "Generate yearly inspections by connection id")
     @PostMapping("/yearly/{connectionId}")
-    public ResponseEntity<?> createYearltInspections(@PathVariable Long connectionId) {
+    public ResponseEntity<?> createYearltInspections(
+            @ApiParam(value = "Unique id of connection", example = "123") @PathVariable Long connectionId) {
         taskService.yearly(connectionId);
         return new ResponseEntity<>(new Message("Yearly inspections have been generated."), HttpStatus.CREATED);
     }
 
     // Generate on demand inspections
+    @ApiOperation(value = "Generate on demand inspections by connection id")
     @PostMapping("/on-demand/{connectionId}")
-    public ResponseEntity<?> createOnDemandInspections(@PathVariable Long connectionId, Principal principal) {
+    public ResponseEntity<?> createOnDemandInspections(
+            @ApiParam(value = "Unique id of connection", example = "123") @PathVariable Long connectionId,
+            @ApiIgnore Principal principal) {
         taskService.onDemand(connectionId, principal);
         return new ResponseEntity<>(new Message("On demand inspections have been generated."), HttpStatus.CREATED);
     }
