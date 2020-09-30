69098139845c1ed55464e3c33bf2d022a61db50a
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-09-30 16:22:20 +0200

    Add get activities by connection test

--- mpgk-backend/src/test/java/com/kozik/MPGK/servicesTests/ActivityServiceTest.java
+++ mpgk-backend/src/test/java/com/kozik/MPGK/servicesTests/ActivityServiceTest.java
@@ -11,10 +11,15 @@ import java.util.Optional;
 
 import com.google.common.collect.Iterables;
 import com.kozik.MPGK.entities.Activity;
+import com.kozik.MPGK.entities.ActivityGroup;
+import com.kozik.MPGK.entities.Connection;
 import com.kozik.MPGK.exceptions.activityExceptions.ActivityAlreadyExistException;
 import com.kozik.MPGK.exceptions.activityExceptions.ActivityNotFoundException;
+import com.kozik.MPGK.repositories.ActivityGroupRepository;
 import com.kozik.MPGK.repositories.ActivityRepository;
 import com.kozik.MPGK.services.ActivityService;
+import com.kozik.MPGK.services.ConnectionService;
+import com.kozik.MPGK.utilities.ActivityObject;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -31,6 +36,12 @@ public class ActivityServiceTest {
 
     @Mock
     ActivityRepository activityRepository;
+
+    @Mock
+    ActivityGroupRepository activityGroupRepository;
+
+    @Mock
+    ConnectionService connectionService;
 
     @InjectMocks
     ActivityService activityService;
@@ -213,4 +224,34 @@ public class ActivityServiceTest {
         activityService.update(activityId, oldActivity);
     }
 
+    @Test
+    public void shouldgetActivitiesByConnectionTest() {
+
+        // Given
+        Long connectionId = 1L;
+        List<Activity> activities1 = Stream
+                .of(new Activity(1L, "name", "type", "emsr", "setting", "list", null, null),
+                        new Activity(2L, "name2", "type2", "emsr2", "setting2", "", null, null))
+                .collect(Collectors.toList());
+        List<Activity> activities2 = Stream
+                .of(new Activity(3L, "name3", "type3", "emsr3", "setting3", "list", null, null),
+                        new Activity(4L, "name4", "type4", "emsr4", "setting4", "", null, null))
+                .collect(Collectors.toList());
+        List<ActivityGroup> activitiesGroups = Stream.of(new ActivityGroup(1L, "first", activities1, null),
+                new ActivityGroup(2L, "second", activities2, null)).collect(Collectors.toList());
+        Connection connection = new Connection(1L, "name", true, null, null, activitiesGroups, null);
+        ActivityObject testObject = new ActivityObject(new ActivityGroup(1L, "first", activities1, null), activities1,
+                true, true);
+        given(connectionService.get(connectionId)).willReturn(connection);
+        given(activityGroupRepository.findByConnection(connection)).willReturn(activitiesGroups);
+        given(activityRepository.findByActivityGroup(activitiesGroups.get(0))).willReturn(activities1);
+        given(activityRepository.findByActivityGroup(activitiesGroups.get(1))).willReturn(activities2);
+
+        // When
+        List<ActivityObject> activityObjects = activityService.getActivitiesByConnection(connectionId);
+
+        // Then
+        assertEquals(2, activityObjects.size());
+        assertEquals(testObject, activityObjects.get(0));
+    }
 }
